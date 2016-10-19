8f4b5ea281e6e027b188362c45368342b62b75a4
Erik Gillespie <erik.gillespie@gmail.com>
2016-10-18 23:19:27 -0400

    Require GitHub OAuth Client ID via test

--- frontend/test/unit/specs/env/env.spec.js
+++ frontend/test/unit/specs/env/env.spec.js
@@ -4,4 +4,8 @@ describe('*.env.js', () => {
   it('has an MSU OAuth Client ID', () => {
     expect(env).to.have.property('msuAuthClientId')
   })
+
+  it('has a GitHub OAuth Client ID', () => {
+    expect(env).to.have.property('githubAuthClientId')
+  })
 })
