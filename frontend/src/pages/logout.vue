b8de796173a3b88f5328d31d79cfd25e43186dfa
Erik Gillespie <erik.gillespie@gmail.com>
2016-10-26 17:13:40 -0400

    Add logout link

---
+++ frontend/src/pages/logout.vue
@@ -0,0 +1,13 @@
+<script>
+import firebase from 'firebase'
+
+export default {
+  beforeRouteEnter (to, from, next) {
+    firebase.auth().signOut()
+    .then(() => {
+      localStorage.removeItem('token')
+      next('/')
+    })
+  }
+}
+</script>
