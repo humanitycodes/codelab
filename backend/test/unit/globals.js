f56307b4e7e9d444f68f88dc372eeb307a96c001
Erik Gillespie <erik.gillespie@gmail.com>
2016-10-27 11:55:58 -0400

    Add mocha unit tests

---
+++ backend/test/unit/globals.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test'
+global.src = '../../../../dist'
+
+const chai = require('chai')
+
+global.expect = chai.expect
+global.assert = chai.assert
+chai.should()
+
+global.sinon = require('sinon')
+
+chai.use(require('sinon-chai'))
