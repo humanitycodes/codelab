db3ee2525314473e25518883ed79db9d5b9de342
Erik Gillespie <erik.gillespie@gmail.com>
2016-10-27 11:55:58 -0400

    Add mocha unit tests

---
+++ backend/test/unit/specs/routes/auth-routes.spec.js
@@ -0,0 +1,23 @@
+import _ from 'lodash'
+
+const routes = require(`${src}/routes/auth-routes`).config
+
+describe('auth-routes', () => {
+  function lookupRoute (method, path) {
+    let route = _.find(routes, (config) => {
+      return config.method === method && config.path === path
+    })
+
+    expect(route).to.exist
+
+    return route
+  }
+
+  it('defines GET /msu/callback', () => {
+    lookupRoute('GET', '/msu/callback')
+  })
+
+  it('defines GET /github/callback', () => {
+    lookupRoute('GET', '/github/callback')
+  })
+})
