de6ea78daa295ae44fc68cf866eb5922a7fa3ac5
Erik Gillespie <erik.gillespie@gmail.com>
2016-11-02 11:58:44 -0400

    Observe user roles and show Lesson link if permitted

--- frontend/src/state/modules/users.js
+++ frontend/src/state/modules/users.js
@@ -3,25 +3,42 @@ import jwtDecode from 'jwt-decode'
 
 export default {
   state: {
-    currentUser: null
+    currentUser: null,
+    userRoles: null
   },
   getters: {
     userSignedIn (state) {
       return !!state.currentUser
+    },
+    userIsStudent (state) {
+      return !!state.currentUser && !!state.userRoles && state.userRoles.student
+    },
+    userIsInstructor (state) {
+      return !!state.currentUser && !!state.userRoles && state.userRoles.instructor
+    },
+    userIsAdmin (state) {
+      return !!state.currentUser && !!state.userRoles && state.userRoles.admin
     }
   },
   actions: {
     syncCurrentUser ({ commit }) {
       firebase.auth().onAuthStateChanged(user => {
         commit('SET_CURRENT_USER', user)
+
+        if (user) {
+          firebase.database().ref(`roles/${user.uid}`)
+          .on('value', roleSnapshot => {
+            commit('SET_CURRENT_ROLES', roleSnapshot.val())
+          })
+        } else {
+          commit('SET_CURRENT_ROLES', null)
+        }
       })
     },
     signIn ({ commit }, token) {
-      const decodedToken = jwtDecode(token)
       return firebase.auth().signInWithCustomToken(token)
         .then(() => {
-          const profile = decodedToken.claims.profile
-          console.log(profile)
+          const profile = jwtDecode(token).claims.profile
           return Promise.all([
             firebase.auth().currentUser.updateEmail(profile.email),
             firebase.auth().currentUser.updateProfile({ displayName: profile.fullName })
@@ -36,6 +53,9 @@ export default {
   mutations: {
     SET_CURRENT_USER (state, newUser) {
       state.currentUser = newUser
+    },
+    SET_CURRENT_ROLES (state, newRoles) {
+      state.userRoles = newRoles
     }
   }
 }
