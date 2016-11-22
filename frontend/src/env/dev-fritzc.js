71e76ef1794143274edadbd868c6259c54e10eaa
Erik Gillespie <erik.gillespie@gmail.com>
2016-11-22 09:40:57 -0500

    Added dev database for fritzc

---
+++ frontend/src/env/dev-fritzc.js
@@ -0,0 +1,11 @@
+import devDefaults from './dev'
+
+export default Object.assign(devDefaults, {
+  firebaseConfig: {
+    apiKey: 'AIzaSyC270-DvezTB0TPy2LgQ7b1dv1TeJ5cwFM',
+    authDomain: 'msulansingcodesdev-fritzc.firebaseapp.com',
+    databaseURL: 'https://msulansingcodesdev-fritzc.firebaseio.com',
+    storageBucket: 'msulansingcodesdev-fritzc.appspot.com',
+    messagingSenderId: '783493485078'
+  }
+})
