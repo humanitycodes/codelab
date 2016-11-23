6f334d27d0a89281c3c1fede2e224a9659061b93
Erik Gillespie <erik.gillespie@gmail.com>
2016-11-23 15:41:22 -0500

    Fix race condition with login and use common signIn for errthing

--- frontend/src/pages/sign-in.vue
+++ frontend/src/pages/sign-in.vue
@@ -3,7 +3,7 @@ import store from '@state/store'
 
 export default {
   beforeRouteEnter (to, from, next) {
-    store.dispatch('signIn', decodeURIComponent(to.query.token))
+    store.dispatch('signIn', { token: decodeURIComponent(to.query.token) })
       .then(() => { window.location.replace('/') })
   }
 }
