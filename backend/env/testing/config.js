ee463cc23ac236695f3ed9134194deb82fb9bfc8
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-01 17:52:44 -0500

    Do not use localhost for external services in test

--- backend/env/testing/config.js
+++ backend/env/testing/config.js
@@ -1,11 +1,11 @@
 export const config = {
   serverBaseURL: 'https://localhost:8080',
 
-  msuAuthBaseURL: 'http://localhost:7777',
+  msuAuthBaseURL: 'https://oauth.ais.msu.edu',
   msuAuthClientID: 'OAuth-MI-MSU-Lansing-Codes-Dev',
   msuAuthClientSecret: 'RhKmT7PVykyBKnutvDYFhSYKM93xyaXysykWQs53',
 
-  githubAuthBaseURL: 'http://localhost:8888',
+  githubAuthBaseURL: 'https://api.github.com',
   githubAuthClientID: '740ecf728a1bf799961b',
   githubAuthClientSecret: '85174d69151083ce58a0c0775324e5f087f1a665'
 }
