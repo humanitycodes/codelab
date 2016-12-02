2e7b96fff6a733221453a1402cf35d3f26ce67ff
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-01 21:22:02 -0500

    Use full path for firebase settings

--- backend/src/firebase-settings.js
+++ backend/src/firebase-settings.js
@@ -1,18 +1,23 @@
 import NodeRSA from 'node-rsa'
 import fs from 'fs'
+import path from 'path'
 
 let firebaseAppConfig
 
+function fullPath (relPath) {
+  return path.resolve(__dirname, relPath)
+}
+
 if (process.env.NODE_ENV === 'production') {
   firebaseAppConfig = {
-    serviceAccount: './env/production/firebase-service-account.json',
+    serviceAccount: fullPath('../env/production/firebase-service-account.json'),
     databaseURL: 'https://msulansingcodes.firebaseio.com'
   }
 } else {
   try {
     // Try to use user-specific dev config
     firebaseAppConfig = {
-      serviceAccount: `./env/dev/firebase-service-account-${process.env.USER}.json`,
+      serviceAccount: fullPath(`../env/dev/firebase-service-account-${process.env.USER}.json`),
       databaseURL: `https://msulansingcodesdev-${process.env.USER}.firebaseio.com`
     }
 
@@ -21,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
   } catch (e) {
     // Use default dev config
     firebaseAppConfig = {
-      serviceAccount: './env/dev/firebase-service-account.json',
+      serviceAccount: fullPath('../env/dev/firebase-service-account.json'),
       databaseURL: 'https://msulansingcodesdev.firebaseio.com'
     }
   }
