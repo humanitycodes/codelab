3e271f52415109a8a49366f72194c68063216e6d
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-05 15:40:34 -0500

    Basic test of student looking at enrolled courses

---
+++ test/e2e/specs/student-course-access.js
@@ -0,0 +1,91 @@
+import uuid from 'uuid'
+
+const db = require('../helpers/db').init()
+
+let studentId = uuid.v4()
+let student = {
+  uid: studentId,
+  email: `${studentId}@test.com`,
+  fullName: 'Test Student'
+}
+
+let instructorId = uuid.v4()
+let instructor = {
+  uid: instructorId,
+  email: `${instructorId}@test.com`,
+  fullName: 'Test Instructor'
+}
+
+let lessonKey = uuid.v4()
+let lesson = {
+  key: lessonKey,
+  createdBy: instructorId,
+  title: 'Test Title',
+  estimatedHours: 4,
+  learningObjectives: {},
+  content: '# Test Content',
+  learningObjectives: {
+    [`${uuid.v4()}`]: {
+      content: 'Learn something'
+    }
+  }
+}
+
+let courseKey = uuid.v4()
+let course = {
+  key: courseKey,
+  createdBy: instructorId,
+  title: 'Test Course',
+  credits: 2,
+  syllabus: 'Test Syllabus',
+  startDate: 'Mon Dec 31 2007 23:59:59 GMT-0500 (EST)',
+  endDate: 'Mon Dec 31 2087 23:59:59 GMT-0500 (EST)',
+  notes: 'Test Notes',
+  lessonKeys: {
+    [`${lessonKey}`]: true
+  },
+  studentKeys: {
+    [`${studentId}`]: true
+  }
+}
+
+module.exports = {
+  before: browser => {
+    db.createStudent(student)
+    db.createInstructor(instructor)
+    db.createLesson(lesson)
+    db.createCourse(course)
+  },
+
+  after: browser => {
+    Promise.all([
+      db.destroyUser(student),
+      db.destroyUser(instructor),
+      db.destroyCourse(course),
+      db.destroyLesson(lesson)
+    ]).then(() => {
+      db.close()
+    })
+  },
+
+  'Enrolled student can access course and lesson': browser => {
+    browser.url(browser.globals.devServerURL)
+      // Sign in
+      .waitForElementVisible('.main-nav a[href^=\'/email-sign-in\']', 5000)
+      .click('.main-nav a[href^=\'/email-sign-in\']')
+      .waitForElementVisible('button', 5000)
+      .setValue('input[type=text]', student.email)
+      .setValue('input[type=password]', db.getDefaultPassword())
+      .click('button')
+      // Navigate to course list
+      .waitForElementVisible('.main-nav a[href^=\'/courses\']', 5000)
+      .click('.main-nav a[href^=\'/courses\']')
+      .refresh() // Content below 'Courses' does not render w/o this. Cannot recreate manually.
+      // Click on the course
+      .waitForElementVisible(`a[href^=\'/courses/${courseKey}\']`, 5000)
+      .click(`a[href^=\'/courses/${courseKey}\']`)
+      .waitForElementVisible('.rendered-content', 5000)
+
+    browser.end()
+  }
+}
