bfcb0b9e9380920cc5efcee116308eb0cbb3b700
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-05 09:51:24 -0500

    Use before/after to setup db

--- test/e2e/specs/sign-in.js
+++ test/e2e/specs/sign-in.js
@@ -2,15 +2,29 @@ import uuid from 'uuid'
 
 import * as db from '../helpers/db'
 
-module.exports = {
-  'Sign In links exist': browser => {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
+let userId = uuid.v4()
+let user = {
+  uid: userId,
+  email: `${userId}@test.com`,
+  fullName: 'Test User'
+}
 
+module.exports = {
+  before: browser => {
+    db.init()
+    db.createUser(user)
+  },
+
+  after: browser => {
+    db.destroyUser(user)
+    .then(() => {
+      db.close()
+    })
+  },
+
+  'Sign In links exist': browser => {
     browser
-      .url(devServer)
+      .url(browser.globals.devServerURL)
       .waitForElementVisible('#container', 5000)
 
     browser.expect.element('.main-nav').to.be.present
@@ -19,16 +33,6 @@ module.exports = {
     browser.end()
   },
   'Successful Sign In with Email shows dashboard': browser => {
-    let userId = uuid.v4()
-    let user = {
-      uid: userId,
-      email: `${userId}@test.com`,
-      fullName: 'Test User'
-    }
-
-    db.init()
-    db.createUser(user)
-
     browser.url(browser.globals.devServerURL)
       .waitForElementVisible('.main-nav a[href^=\'/email-sign-in\']', 5000)
       .click('.main-nav a[href^=\'/email-sign-in\']')
@@ -37,10 +41,6 @@ module.exports = {
       .setValue('input[type=password]', db.USER_PASSWORD)
       .click('button')
       .waitForElementVisible('.main-nav a[href^=\'/sign-out\']', 5000)
-      .end(() => {
-        db.destroyUser(user).then(() => {
-          db.close()
-        })
-      })
+      .end()
   }
 }
