1bbce069851d2ec1270cc28512156e6ff312d751
Chris Fritz <chrisvfritz@gmail.com>
2016-12-06 18:27:02 -0500

    add comments to firebase adapter and fix a bug

--- firebase/src/adapter.js
+++ firebase/src/adapter.js
@@ -13,6 +13,9 @@ const capitalize = word => {
 }
 
 export default (store, db, uid, roles) => {
+  // In case there are no roles because no user is signed in
+  roles = roles || {}
+  // Cache to keep track of which firebase stuff we've already synced
   const alreadySyncedCache = {}
   return {
     state: {
@@ -23,20 +26,25 @@ export default (store, db, uid, roles) => {
       })
     },
     getters: {
+      // For each resource defined in defs/resources
       ...mapAndMerge(resourceNames, resourceName => {
         const singularResourceName = singularize(resourceName)
         const currentResourceName = 'current' + capitalize(singularResourceName)
         const currentResourcePathName = capitalize(`${currentResourceName}Path`)
         return {
+          // E.g. "lessons"
           [resourceName] (state, getters, rootState) {
             const resourceArray = []
             const stateDefs = state[`raw-${resourceName}`]
             const resourceKeys = Object.keys(stateDefs)
+            // For each of the keys that we want to fetch information for
             resourceKeys.forEach((resourceKey, resourceIndex) => {
               const resourceItem = {}
+              // Define the key as read-only
               Object.defineProperty(resourceItem, '.key', {
                 value: resourceKey
               })
+              // Define the meta fields as read-only
               const meta = stateDefs[resourceKey].meta
               const metaFields = Object.keys(meta)
               metaFields.forEach(metaField => {
@@ -44,6 +52,7 @@ export default (store, db, uid, roles) => {
                   value: meta[metaField]
                 })
               })
+              // Define relationships as read-only
               const { relationships } = resourceDefs[resourceName]
               const relationshipNames = Object.keys(relationships)
               relationshipNames.forEach(relationshipName => {
@@ -64,7 +73,10 @@ export default (store, db, uid, roles) => {
                   : resourceName
                 const singularRelatedResourceName = singularize(relationshipName)
                 const addRelatedResourceName = 'add' + capitalize(singularRelatedResourceName)
+                // Define "add" method for the relationship
+                // E.g. course.addLesson(lessonKey)
                 resourceItem[addRelatedResourceName] = function (relatedResourceKey) {
+                  // Add relationship with meta keys to current record
                   db.ref(`${resourceName}/relationships`)
                     .child(resourceKey)
                     .child(relationshipName)
@@ -77,7 +89,11 @@ export default (store, db, uid, roles) => {
                     .child(relatedResourceKey)
                     .child('createdBy')
                     .set(uid)
-                  const resourceNames = Object.keys(resourceDefs)
+                  // If the resource is listed in defs/resources, add
+                  // the relationship there as well to create a two-way
+                  // relationship (i.e. many-to-many)
+                  // E.g. When you add a lesson to a course, you are
+                  // also adding that course to the lesson
                   if (resourceNames.indexOf(relatedResourceName) !== -1) {
                     db.ref(`${relatedResourceName}/relationships`)
                       .child(relatedResourceKey)
@@ -93,14 +109,21 @@ export default (store, db, uid, roles) => {
                       .set(uid)
                   }
                 }
+                // Define "remove" method for the relationship
+                // E.g. course.removeLesson(lessonKey)
                 const removeRelatedResourceName = 'remove' + capitalize(singularRelatedResourceName)
                 resourceItem[removeRelatedResourceName] = function (relatedResourceKey) {
+                  // Remove relationship from the current record
                   db.ref(`${resourceName}/relationships`)
                     .child(resourceKey)
                     .child(relationshipName)
                     .child(relatedResourceKey)
                     .remove()
-                  const resourceNames = Object.keys(resourceDefs)
+                  // If the resource is listed in defs/resources, remove
+                  // the relationship there as well to end a two-way
+                  // relationship (i.e. many-to-many)
+                  // E.g. When you remove a lesson from a course, you are
+                  // also removing that course from the lesson
                   if (resourceNames.indexOf(relatedResourceName) !== -1) {
                     db.ref(`${relatedResourceName}/relationships`)
                       .child(relatedResourceKey)
@@ -112,18 +135,28 @@ export default (store, db, uid, roles) => {
               })
               const sizeDef = resourceDefs[resourceName].fieldGroups
               const sizeNames = Object.keys(sizeDef)
+              // Loop through any possible sizes for that resource
+              // E.g. small, large
               sizeNames.forEach(sizeName => {
                 const groupDef = sizeDef[sizeName]
                 const groupNames = Object.keys(groupDef)
+                // Loop through any possible field groups for that resource
+                // E.g. authed, student, instructor
                 groupNames.forEach(groupName => {
                   const fields = groupDef[groupName]
                   const fieldNames = Object.keys(fields)
+                  // Loop through any possible fields for that resource
+                  // E.g. title, syllabus
                   fieldNames.forEach(fieldName => {
+                    // Get the value that we have stored locally
+                    // in our state, if any for that field
                     const fieldValue = (
                       stateDefs[resourceKey][sizeName] &&
                       stateDefs[resourceKey][sizeName][groupName] &&
                       stateDefs[resourceKey][sizeName][groupName][fieldName]
                     ) || undefined
+                    // Define the field as on the item/record, with a setter
+                    // that actually updates the field in Firebase, yo!!!
                     Object.defineProperty(resourceItem, fieldName, {
                       get: () => fieldValue,
                       set (newValue) {
@@ -149,9 +182,13 @@ export default (store, db, uid, roles) => {
                   })
                 })
               })
+              // The item/record is done being built, add it to the resourceArray
               resourceArray.push(resourceItem)
             })
+            // Creates a new record in Firebase with a key
+            // E.g. lessons.add(lessonKey)
             resourceArray.add = function (itemKey) {
+              // Initialize record with metadata
               return Promise.all([
                 db.ref(resourceName)
                   .child('meta')
@@ -165,14 +202,18 @@ export default (store, db, uid, roles) => {
                   .set(uid)
               ])
             }
+            // Deletes an existing record in Firebase with a key
+            // E.g. lessons.remove(lessonKey)
             resourceArray.remove = function (itemKey) {
               const firebaseActions = []
+              // Delete metadata for record in Firebase
               firebaseActions.push(
                 db.ref(resourceName)
                   .child('meta')
                   .child(itemKey)
                   .remove()
               )
+              // Delete all fields for a record in Firebase
               const sizes = resourceDefs[resourceName].fieldGroups
               const sizeNames = Object.keys(sizes)
               sizeNames.forEach(sizeName => {
@@ -188,11 +229,14 @@ export default (store, db, uid, roles) => {
                   )
                 })
               })
-              // TODO: Also delete relationships, by pulling in all relationships of the deleted resource that exist and deleting their inverse
+              // TODO: Also delete relationships, by pulling in all
+              // relationships of the deleted resource that exist
+              // and deleting their inverse
               return Promise.all(firebaseActions)
             }
             return resourceArray
           },
+          // E.g. "currentLesson"
           [currentResourceName] (state, getters, rootState) {
             const resourceKey = rootState.route.params[`${singularResourceName}Key`]
             store.dispatch('syncLargeFieldsOfResource', {
@@ -202,10 +246,12 @@ export default (store, db, uid, roles) => {
               return resource['.key'] === resourceKey
             })
           },
+          // E.g. "showCurrentLessonPath"
           [`show${currentResourcePathName}`] (state, getters, rootState) {
             const currentResourceKey = getters[currentResourceName]['.key']
             return `/${resourceName}/${currentResourceKey}`
           },
+          // E.g. "editCurrentLessonPath"
           [`edit${currentResourcePathName}`] (state, getters, rootState) {
             const currentResourceKey = getters[currentResourceName]['.key']
             return `/${resourceName}/${currentResourceKey}/edit`
@@ -214,21 +260,30 @@ export default (store, db, uid, roles) => {
       })
     },
     actions: {
+      // Sync all the resources except for users and roles
       syncResources ({ rootState, commit }) {
+        // Skip syncing if we've already synced once
         if (alreadySyncedCache.allResources) return Promise.resolve()
         alreadySyncedCache.allResources = true
+        // For all of the resources defined in defs/resources
         return Promise.all(resourceNames.map(resourceName => {
+          // We download all the small fields for every record,
+          // but only the small fields, because we it might be a lot of data
           const smallFieldGroups = resourceDefs[resourceName].fieldGroups.small
           const smallFieldGroupNames = Object.keys(smallFieldGroups)
           return new Promise((resolve, reject) => {
+            // If no user is signed in, the resource will just be an empty array
             if (!uid) return resolve([])
+            // This is where we store the firebase ref for
+            // the items/records we'll need to fetch
             let itemsToFetchRef
             if (roles.instructor) {
               // Fetch all existing resources
               itemsToFetchRef = db
                 .ref(`${resourceName}/meta`)
             } else {
-              // Fetch just the resources the user has access to
+              // Fetch just the resources the user has access to,
+              // if they're not an instructor
               itemsToFetchRef = db
                 .ref(`${resourceName}/relationships`)
                 .orderByChild(`student/${uid}`)
@@ -236,13 +291,17 @@ export default (store, db, uid, roles) => {
                 // https://firebase.google.com/docs/database/web/lists-of-data#sorting_and_filtering_data#orderbychild
                 .startAt(true)
             }
+            // Fetch all of the keys that we're going to pull in data for
             itemsToFetchRef.once('value', snapshot => {
               const resources = snapshot.val()
               if (!resources) return resolve([])
               const resourceKeys = Object.keys(resources)
               resolve(resourceKeys)
             })
+          // For all the records that the user has access to
           }).then(keysToSync => {
+            // Select the subset of field groups that the user
+            // has access to
             const accessibleFieldGroupNames = smallFieldGroupNames
               .filter(fieldGroupName => {
                 return (
@@ -252,7 +311,9 @@ export default (store, db, uid, roles) => {
                   ['public', 'authed', 'student'].includes(fieldGroupName)
                 )
               })
+            // Collecting pieces of each record
             return Promise.all(flatten([
+              // Collecting all the relationships
               keysToSync.map(resourceKey => {
                 const relationshipDefs = resourceDefs[resourceName].relationships
                 const relationshipNames = Object.keys(relationshipDefs)
@@ -273,6 +334,7 @@ export default (store, db, uid, roles) => {
                   })
                 )
               }),
+              // Collecting all the meta properties
               keysToSync.map(resourceKey => {
                 new Promise((resolve, reject) => {
                   db.ref(`${resourceName}/meta`)
@@ -286,6 +348,7 @@ export default (store, db, uid, roles) => {
                     })
                 })
               }),
+              // Collecting all the small fields
               keysToSync.map(resourceKey => {
                 accessibleFieldGroupNames.map(fieldGroupName => {
                   return new Promise((resolve, reject) => {
@@ -306,6 +369,7 @@ export default (store, db, uid, roles) => {
           })
         }))
       },
+      // Lazily sync large fields when we need them
       syncLargeFieldsOfResource ({ commit }, { resourceName, resourceKey }) {
         alreadySyncedCache[resourceName] = alreadySyncedCache[resourceName] || {}
         if (alreadySyncedCache[resourceName][resourceKey]) return
@@ -333,6 +397,12 @@ export default (store, db, uid, roles) => {
       }
     },
     mutations: {
+      // Store a resource item in a kind of mirror image of how
+      // we're storing the data in Firebase. For example, if we
+      // have this in Firebase:
+      //    /lessons/small/instructor/:lessonsKey
+      // Then we would store it locally as:
+      //    /lessons/:lessonsKey/small/instructor/
       SET_RESOURCE_ITEM (state, data) {
         const pathSegments = data.path.split('/')
         const stateResource = state[`raw-${pathSegments.shift()}`]
