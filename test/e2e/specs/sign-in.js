776ba7c6831472c3fc3eab21571732b572c07647
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-08 09:28:53 -0500

    Put test data generation in a common place

--- test/e2e/specs/sign-in.js
+++ test/e2e/specs/sign-in.js
@@ -1,13 +1,7 @@
-const uuid = require('uuid')
-
 const db = require('../helpers/db').init()
+const dgen = require('../helpers/data-generator')
 
-let userId = uuid.v4()
-let user = {
-  uid: userId,
-  email: `${userId}@test.com`,
-  fullName: 'Test User'
-}
+const user = dgen.user()
 
 module.exports = {
   before: browser => {
@@ -30,8 +24,10 @@ module.exports = {
     browser.expect.element('.main-nav a[href^=\'/email-sign-in\']').to.be.present
     browser.end()
   },
+
   'Successful Sign In with Email shows dashboard': browser => {
-    browser.url(browser.globals.devServerURL)
+    browser
+      .url(browser.globals.devServerURL)
       .waitForElementVisible('.main-nav a[href^=\'/email-sign-in\']', 5000)
       .click('.main-nav a[href^=\'/email-sign-in\']')
       .waitForElementVisible('button', 5000)
