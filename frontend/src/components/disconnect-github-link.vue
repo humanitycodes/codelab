a41583ef0ad184acfd0f40fda49952498fcfa5b0
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-09 09:56:07 -0500

    Disconnect Github account

---
+++ frontend/src/components/disconnect-github-link.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="disconnectGithub(currentUser)" class="inline danger">
+    <slot/>
+  </button>
+</template>
+
+<script>
+import { userGetters } from '@state/helpers'
+
+export default {
+  methods: {
+    disconnectGithub (user) {
+      user.github = null
+    }
+  },
+  computed: userGetters
+}
+</script>
--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -4,17 +4,22 @@
       Welcome Home
       <span v-if="userSignedIn">{{ currentUser.fullName }}</span>
     </h1>
-    <h3 v-if="currentUser.github">GitHub User: {{ currentUser.github.login }}</h3>
+    <div v-if="userSignedIn && currentUser.github">
+      <strong>GitHub User:</strong>
+      {{ currentUser.github.login }}
+      <DisconnectGithubLink>X</DisconnectGithubLink>
+    </div>
   </Layout>
 </template>
 
 <script>
 import { userGetters } from '@state/helpers'
 import Layout from '@layouts/main'
+import DisconnectGithubLink from '@components/disconnect-github-link'
 
 export default {
   components: {
-    Layout
+    Layout, DisconnectGithubLink
   },
   computed: userGetters
 }
