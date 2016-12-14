356abfdf63a14940f0bdc0ba9f06979c1076652b
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-14 16:36:12 -0500

    Verify edited course fields

--- test/e2e/specs/instructor-course-access.js
+++ test/e2e/specs/instructor-course-access.js
@@ -1,4 +1,5 @@
 const randomatic = require('randomatic')
+const datefns = require('date-fns')
 
 const db = require('../helpers/db').init()
 const dgen = require('../helpers/data-generator')
@@ -72,10 +73,12 @@ module.exports = {
       .setValue(`input[name=lesson-estimated-hours]`, lesson.estimatedHours)
       .setValue(`textarea[name=lesson-content]`, lesson.content)
       .setValue(`textarea[name=lesson-notes]`, lesson.notes)
+
       .setValue(`input[name=lesson-new-learning-objective]`, [
         Object.values(lesson.learningObjectives)[0].content,
         browser.Keys.ENTER
       ])
+
       .setValue(`input[name=lesson-prereq-query]`, prereqLesson.title)
       .waitForElementVisible('input[name=lesson-prereq-query] + .dropdown-results > .dropdown-result', 5000)
       .click('input[name=lesson-prereq-query] + .dropdown-results > .dropdown-result')
@@ -129,36 +132,40 @@ module.exports = {
       .click('button:not([DISABLED])')
       .waitForElementVisible('input[name=course-title]', 5000)
       .assert.urlContains(`/courses/${course.key}/edit`)
-/*
+
       // Edit course fields
-      .setValue(`input[name=lesson-title]`, lesson.title)
-      .setValue(`input[name=lesson-estimated-hours]`, lesson.estimatedHours)
-      .setValue(`textarea[name=lesson-content]`, lesson.content)
-      .setValue(`textarea[name=lesson-notes]`, lesson.notes)
-      .setValue(`input[name=lesson-new-learning-objective]`, [
-        Object.values(lesson.learningObjectives)[0].content,
-        browser.Keys.ENTER
-      ])
-      .setValue(`input[name=lesson-prereq-query]`, prereqLesson.title)
-      .waitForElementVisible('input[name=lesson-prereq-query] + .dropdown-results > .dropdown-result', 5000)
-      .click('input[name=lesson-prereq-query] + .dropdown-results > .dropdown-result')
+      .setValue(`input[name=course-title]`, course.title)
+      .setValue(`textarea[name=course-syllabus]`, course.syllabus)
+      .setValue(`input[name=course-credits]`, course.credits)
+
+      // setValue sends keystrokes, so format the dates in the order you would type a date
+      .setValue(`input[name=course-start-date]`, datefns.format(course.startDate, 'MMDDYYYY'))
+      .setValue(`input[name=course-end-date]`, datefns.format(course.endDate, 'MMDDYYYY'))
+
+      .setValue(`input[name=course-student-query]`, student.fullName)
+      .waitForElementVisible('input[name=course-student-query] + .dropdown-results > .dropdown-result', 5000)
+      .click('input[name=course-student-query] + .dropdown-results > .dropdown-result')
+
+      .setValue(`input[name=course-lesson-query]`, lesson.title)
+      .waitForElementVisible('input[name=course-lesson-query] + .dropdown-results > .dropdown-result', 5000)
+      .click('input[name=course-lesson-query] + .dropdown-results > .dropdown-result')
 
       // Make sure the course is in the master list
-      .click(`.main-nav a[href^='/lessons']`)
-      .waitForElementVisible(`a[href^='/lessons/new']`, 5000)
-      .assert.visible(`a[href^='/lessons/${lesson.key}/edit']`)
+      .click(`.main-nav a[href^='/courses']`)
+      .waitForElementVisible(`a[href^='/courses/new']`, 5000)
+      .assert.visible(`a[href^='/courses/${course.key}/edit']`)
 
       // View the recently edited course
-      .click(`a[href^='/lessons/${lesson.key}/edit']`)
-      .waitForElementVisible('input[name=lesson-title]', 5000)
-      .assert.value(`input[name=lesson-title]`, lesson.title)
-      .assert.value(`input[name=lesson-estimated-hours]`, lesson.estimatedHours.toString())
-      .assert.value(`textarea[name=lesson-content]`, lesson.content)
-      .assert.value(`textarea[name=lesson-notes]`, lesson.notes)
-      .assert.value(`input[name=lesson-new-learning-objective] + ol input`,
-        Object.values(lesson.learningObjectives)[0].content)
-      .assert.visible(`button[name=lesson-delete-prereq]`)
-*/
+      .click(`a[href^='/courses/${course.key}/edit']`)
+      .waitForElementVisible('input[name=course-title]', 5000)
+      .assert.value(`input[name=course-title]`, course.title)
+      .assert.value(`textarea[name=course-syllabus]`, course.syllabus)
+      .assert.value(`input[name=course-credits]`, course.credits.toString())
+      .assert.value(`input[name=course-start-date]`, datefns.format(course.startDate, 'YYYY-MM-DD'))
+      .assert.value(`input[name=course-end-date]`, datefns.format(course.endDate, 'YYYY-MM-DD'))
+      .assert.visible(`button[name=course-remove-student]`)
+      .assert.visible(`button[name=course-remove-lesson]`)
+
       // Close the browser and end the test
       .end()
   }
