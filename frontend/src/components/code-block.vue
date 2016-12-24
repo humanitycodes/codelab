5a454330df2ceb983e1a793ab16945314d56b18d
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-24 09:47:38 -0500

    Code block component

---
+++ frontend/src/components/code-block.vue
@@ -0,0 +1,14 @@
+<template>
+  <pre><code>{{ this.$slots.default[0].text.trim() }}</code></pre>
+</template>
+
+<style lang="stylus" scoped>
+@import '../meta'
+
+pre
+  padding: 1rem
+  background-color: $design.branding.muted.light.gray
+  > code
+    display: block
+    font-weight: bold
+</style>
