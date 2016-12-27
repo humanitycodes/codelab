bf5faf066ef94fee504df458230059ebc337dba6
Erik Gillespie <erik.gillespie@gmail.com>
2016-12-27 13:02:48 -0500

    Create and receive GitHub webhooks

--- backend/env/testing/config.js
+++ backend/env/testing/config.js
@@ -7,5 +7,8 @@ module.exports = {
 
   githubAuthBaseURL: 'https://api.github.com',
   githubAuthClientID: '740ecf728a1bf799961b',
-  githubAuthClientSecret: '85174d69151083ce58a0c0775324e5f087f1a665'
+  githubAuthClientSecret: '85174d69151083ce58a0c0775324e5f087f1a665',
+
+  githubEventsBaseURL: 'https://msu.lansing.codes',
+  githubEventsPath: '/github-events/eb564a2f-fca6-41fa-8e6d-11009a330e0b'
 }
