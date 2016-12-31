e83ff5771da9831b21e62db9b5e58cbcf7cbd80a
Chris Fritz <chrisvfritz@gmail.com>
2016-12-31 18:18:50 -0500

    add hosting select for lesson projects

--- frontend/src/components/lesson-form-project.vue
+++ frontend/src/components/lesson-form-project.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="flex-row" v-if="project">
     <div class="flex-col">
-      <label>Project</label>
+      <label>Project Title</label>
       <input
         v-model="project.title"
         placeholder="Project title"
         class="lesson-project-title-input"
       >
+      <label>Project Hosting</label>
+      <select v-model="project.hosting">
+        <option
+          v-for="hostingOption in hostingOptions"
+          :value="hostingOption"
+        >{{ hostingOption }}</option>
+      </select>
       <p v-if="!project.title" class="warning">
         This lesson's project needs a title so that people will understand the general idea of what they'll be building.
       </p>
+      <label>Project Criteria</label>
       <input
         v-model="newProjectCriterion"
         @keydown.enter="addCriterion"
@@ -42,12 +50,20 @@ export default {
   },
   data () {
     return {
-      newProjectCriterion: ''
+      newProjectCriterion: '',
+      hostingOptions: [
+        'GitHub Pages',
+        'Heroku'
+      ]
     }
   },
   computed: {
     project () {
       if (!this.lesson.projects.length) return
+      const project = this.lesson.projects[0]
+      if (!project.hosting) {
+        project.hosting = this.hostingOptions[0]
+      }
       return this.lesson.projects[0]
     },
     largeFieldsLoaded () {
