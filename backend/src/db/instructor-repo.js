94c29218eba7948c476de2268e8e5a453d5ec3b9
Erik Gillespie <erik.gillespie@gmail.com>
2017-01-02 16:08:29 -0500

    Don't include instructor who created course in list

--- backend/src/db/instructor-repo.js
+++ backend/src/db/instructor-repo.js
@@ -5,36 +5,29 @@ import { readUserById } from './user-repo'
 export async function readInstructorsByCourseKey (courseKey) {
   const db = firebase.database()
 
-  return Promise.all([
-    db.ref('courses/meta')
-      .child(courseKey)
-      .child('createdBy')
-      .once('value'),
-    db.ref('courses/relationships')
-      .child(courseKey)
-      .child('instructors')
-      .once('value')
-  ])
-  .then(([createdBySnapshot, instructorsSnapshot]) => {
-    let instructorLookups = []
-    instructorLookups.push(readUserById(createdBySnapshot.val()))
-    if (instructorsSnapshot.val()) {
-      Object.keys(instructorsSnapshot.val()).forEach(uid => {
-        instructorLookups.push(readUserById(uid))
-      })
-    }
-    return Promise.all(instructorLookups)
-  })
-  .then(instructorResults => {
-    let instructors = {}
-    instructorResults.forEach(([uid, user]) => {
-      if (user.github) {
-        delete user.github.scope
-        delete user.github.token
-        delete user.github.tokenType
+  return db.ref('courses/relationships')
+    .child(courseKey)
+    .child('instructors')
+    .once('value')
+    .then(instructorsSnapshot => {
+      let instructorLookups = []
+      if (instructorsSnapshot.val()) {
+        Object.keys(instructorsSnapshot.val()).forEach(uid => {
+          instructorLookups.push(readUserById(uid))
+        })
       }
-      instructors[uid] = user
+      return Promise.all(instructorLookups)
     })
-    return instructors
-  })
+    .then(instructorResults => {
+      let instructors = {}
+      instructorResults.forEach(([uid, user]) => {
+        if (user.github) {
+          delete user.github.scope
+          delete user.github.token
+          delete user.github.tokenType
+        }
+        instructors[uid] = user
+      })
+      return instructors
+    })
 }
