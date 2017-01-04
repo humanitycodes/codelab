81ddcab6afbde8119f6bcec67cc228409ab2a32f
Chris Fritz <chrisvfritz@gmail.com>
2017-01-04 01:08:40 -0500

    add basic editing interface with live preview for course syllabus and lesson content

---
+++ frontend/src/components/rich-content-editor.vue
@@ -0,0 +1,128 @@
+<template>
+  <div
+    class="rich-content-editor-container"
+    :class="{ expanded: isExpanded }"
+  >
+    <textarea
+      ref="textarea"
+      @input="$emit('input', $event.target.value)"
+    />
+    <RenderedContent
+      v-if="isExpanded"
+      pagination-placement="top"
+      :content="value"
+    />
+  </div>
+</template>
+
+<script>
+import 'codemirror/lib/codemirror.css'
+import '@assets/css/codemirror-theme-one-dark.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/mode/gfm/gfm'
+import CodeMirror from 'codemirror/lib/codemirror.js'
+import RenderedContent from './rendered-content'
+
+export default {
+  components: {
+    RenderedContent
+  },
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      editor: null,
+      isExpanded: false
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.editor = CodeMirror.fromTextArea(this.$refs.textarea, {
+        mode: 'gfm',
+        theme: 'one-dark',
+        lineNumbers: false,
+        tabSize: 2,
+        lineWrapping: true,
+        extraKeys: {
+          Tab: editor => {
+            var spaces = Array(editor.getOption('indentUnit') + 1).join(' ')
+            editor.replaceSelection(spaces)
+          },
+          Esc: editor => {
+            this.collapseEditor()
+          }
+        }
+      })
+      this.editor.setValue(this.value)
+      this.editor.on('change', editor => {
+        this.$emit('input', editor.getValue())
+      })
+      this.editor.on('focus', this.expandEditor)
+    })
+  },
+  watch: {
+    value (newValue) {
+      if (this.editor.getValue() !== newValue) {
+        this.editor.setValue(newValue)
+      }
+    }
+  },
+  methods: {
+    expandEditor () {
+      if (this.isExpanded) return
+      this.isExpanded = true
+      document.body.style.overflowY = 'hidden'
+      window.addEventListener('keydown', this.collapseEditorIfEscapeKey)
+    },
+    collapseEditor () {
+      if (!this.isExpanded) return
+      this.isExpanded = false
+      document.body.style.overflowY = 'auto'
+      window.removeEventListener('keydown', this.collapseEditorIfEscapeKey)
+    },
+    collapseEditorIfEscapeKey (event) {
+      if (event.keyCode === 27) {
+        this.collapseEditor()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '../meta'
+
+.rich-content-editor-container
+  .CodeMirror
+    border-radius: $design.control.border.radius
+    pre
+      font-size: 16px
+      font-family: $design.code.font.family
+    .CodeMirror-lines
+      padding: $design.layout.gutterWidth
+
+  &.expanded
+    .CodeMirror, .rendered-content-container
+      position: fixed
+      top: 0
+      width: 50%
+      height: 100%
+      border-radius: 0
+      z-index: 10
+    .CodeMirror
+      left: 0
+    .rendered-content-container
+      right: 0
+      background-color: #FFF
+      padding: $design.layout.gutterWidth
+      > .rendered-content
+        flex-grow: 1
+</style>
