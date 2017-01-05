5f9fd1d0c6feb748e80e4d6660c020e0c095c6d0
Erik Gillespie <erik.gillespie@gmail.com>
2017-01-05 10:50:19 -0500

    Remove lame test

--- backend/test/unit/specs/routes/auth-routes.spec.js
+++ backend/test/unit/specs/routes/auth-routes.spec.js
@@ -4,8 +4,4 @@ describe('auth-routes', () => {
   it('routes GET /msu/callback', () => {
     expect(routes).to.resolve('GET', '/msu/callback')
   })
-
-  it('routes GET /github/callback', () => {
-    expect(routes).to.resolve('GET', '/github/callback')
-  })
 })
