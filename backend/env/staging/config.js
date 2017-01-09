6819abcb3646dce1ec0327c9adbdf825c206235c
Erik Gillespie <erik.gillespie@gmail.com>
2017-01-09 10:35:12 -0500

    Changes for staging env

---
+++ backend/env/staging/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  serverBaseURL: 'https://staging.msu.codes',
+
+  msuAuthBaseURL: 'https://oauth.ais.msu.edu',
+  msuAuthClientID: '',
+  msuAuthClientSecret: '',
+
+  githubAuthBaseURL: 'https://api.github.com',
+  githubAuthClientID: '473069becdcae15254ea',
+  githubAuthClientSecret: '2973bb74e05293f369e3c9bd6ffd2bfbe259cd28',
+
+  githubEventsBaseURL: 'https://staging.msu.codes/api',
+  githubEventsPath: '/github-events/53ba4eac-2d46-4ce8-a8e2-1d70da0da9d2'
+}
