209e876788f7febcb56f8687423df846120bebae
Erik Gillespie <erik.gillespie@gmail.com>
2017-01-09 10:35:26 -0500

    Changes for staging env

---
+++ frontend/src/env/staging.js
@@ -0,0 +1,13 @@
+export default {
+  msuAuthClientId: '',
+  githubAuthClientId: '473069becdcae15254ea',
+  githubAuthRedirectURL: 'https://staging.msu.codes/auth/github/callback',
+
+  firebaseConfig: {
+    apiKey: "AIzaSyC_oQhUboMjMfX38CaApt28rIQO2rAq_88",
+    authDomain: "msulansingcodesstaging.firebaseapp.com",
+    databaseURL: "https://msulansingcodesstaging.firebaseio.com",
+    storageBucket: "msulansingcodesstaging.appspot.com",
+    messagingSenderId: "508904669695"
+  }
+}
