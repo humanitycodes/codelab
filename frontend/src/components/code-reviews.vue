6ded2b8048871a227c9c392df4c125099b439069
Erik Gillespie <erik.gillespie@gmail.com>
2017-01-12 16:13:01 -0500

    Code review dashboard

---
+++ frontend/src/components/code-reviews.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="flex-row">
+    <div class="flex-col">
+      <div v-for="(codeReviews, instructorKey) in codeReviewsAwaitingFeedback">
+        <h2>{{ getUser(instructorKey).fullName }}</h2>
+        <ul>
+          <li v-for="codeReview in codeReviews">
+            {{ codeReview.course['.key'] }}
+            -
+            <router-link :to="'/lessons/' + codeReview.lesson['.key']">
+              {{ codeReview.lesson.title }}
+            </router-link>
+            <br/>
+            {{ codeReview.student.fullName }}
+            (<a :href="'mailto:' + codeReview.student.email" target="_blank">{{ codeReview.student.email }}</a>)
+            <br/>
+            <a :href="getIssuesUrl(codeReview)" target="_blank">GitHub Issue</a>
+          </li>
+        </ul>
+      </div>
+      <div v-if="!Object.keys(codeReviewsAwaitingFeedback).length">
+        There are no code reviews awaiting feedback.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  coursePermissionMethods, userGetters, lessonGetters
+} from '@state/helpers'
+import store from '@state/store'
+
+export default {
+  props: {
+    courses: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    ...coursePermissionMethods,
+    getUser (userKey) {
+      return this.users.find(user => user['.key'] === userKey)
+    },
+    getLesson (lessonKey) {
+      return this.lessons.find(lesson => lesson['.key'] === lessonKey)
+    },
+    getIssuesUrl (codeReview) {
+      return [
+        'https://github.com/',
+        codeReview.student.github.login,
+        '/',
+        [
+          codeReview.course['.key'],
+          codeReview.lesson['.key'],
+          codeReview.project.projectKey.slice(-6)
+        ].join('-'),
+        '/issues'
+      ].join('')
+    }
+  },
+  created () {
+    // Retrieve the large fields so projectCompletions are available
+    this.courses.forEach(course => {
+      store.dispatch('syncLargeFieldsOfResource', {
+        resourceName: 'courses',
+        resourceKey: course['.key']
+      })
+    })
+  },
+  computed: {
+    ...userGetters,
+    ...lessonGetters,
+    codeReviewsAwaitingFeedback () {
+      let codeReviews = {}
+      this.courses.forEach(course => {
+        course.projectCompletions.forEach(project => {
+          if (!project.submission || project.submission.instructorCommentedLast) return
+
+          if (!codeReviews[project.submission.assignedInstructor]) {
+            codeReviews[project.submission.assignedInstructor] = []
+          }
+
+          codeReviews[project.submission.assignedInstructor].push({
+            course,
+            project,
+            lesson: this.getLesson(project.lessonKey),
+            student: this.getUser(project.students[0]['.key'])
+          })
+        })
+      })
+      return codeReviews
+    }
+  }
+}
+</script>
---
+++ frontend/src/pages/instructor-dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>Code Reviews</h1>
+    <CodeReviews :courses="courses"/>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@layouts/main'
+import CodeReviews from '@components/code-reviews'
+import { courseGetters } from '@state/helpers'
+
+export default {
+  components: {
+    Layout, CodeReviews
+  },
+  computed: courseGetters
+}
+</script>
