298abb7d3fe8261fced7ee5a731aef9681f20429
Erik Gillespie <erik.gillespie@gmail.com>
2017-01-20 20:37:03 -0500

    Loop over sorted courses

--- frontend/src/pages/instructor-dashboard.vue
+++ frontend/src/pages/instructor-dashboard.vue
@@ -2,6 +2,11 @@
   <Layout>
     <h1>Code Reviews</h1>
     <CodeReviews :courses="courses"/>
+
+    <h1>Course Progress</h1>
+    <div v-for="course in sortByCourseKey(courses)">
+      <h2>{{ course['.key'] }}</h2>
+    </div>
   </Layout>
 </template>
 
@@ -9,10 +14,16 @@
 import Layout from '@layouts/main'
 import CodeReviews from '@components/code-reviews'
 import { courseGetters } from '@state/helpers'
+import sortBy from 'lodash/sortBy'
 
 export default {
   components: {
     Layout, CodeReviews
+  },
+  methods: {
+    sortByCourseKey (courses) {
+      return sortBy(courses, [course => course['.key']])
+    }
   },
   computed: courseGetters
 }
