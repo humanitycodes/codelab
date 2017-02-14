d677e85c4311a0033b6d48b3a42c66b3c073d360
Erik Gillespie <erik.gillespie@gmail.com>
2017-02-13 21:40:36 -0500

    Include end date, add tests

--- frontend/src/helpers/total-days-in-course.js
+++ frontend/src/helpers/total-days-in-course.js
@@ -2,6 +2,6 @@ import differenceInDays from 'date-fns/difference_in_days'
 
 export default (course) => {
   const { startDate, endDate } = course
-  const totalDays = differenceInDays(endDate, startDate)
+  const totalDays = differenceInDays(endDate, startDate) + 1
   return Math.max(0, totalDays)
 }
