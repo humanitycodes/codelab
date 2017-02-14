a2566f4561b46c5a4ed8b469dbd8282415a97af9
Erik Gillespie <erik.gillespie@gmail.com>
2017-02-14 11:00:59 -0500

    Include current date in days in course, add unit tests

--- frontend/src/helpers/days-so-far-in-course.js
+++ frontend/src/helpers/days-so-far-in-course.js
@@ -2,6 +2,6 @@ import differenceInDays from 'date-fns/difference_in_days'
 
 export default (course) => {
   const { startDate } = course
-  const daysSoFar = differenceInDays(Date.now(), startDate)
+  const daysSoFar = differenceInDays(Date.now(), startDate) + 1
   return Math.max(0, daysSoFar)
 }
