3a0cabd0eb512c2afa3966c7519f681ee8c9cb2d
Erik Gillespie <erik.gillespie@gmail.com>
2017-02-15 17:19:35 -0500

    Use days left in course to calculate min expected grade, add tests

--- frontend/src/helpers/min-grade-expectation.js
+++ frontend/src/helpers/min-grade-expectation.js
@@ -4,7 +4,10 @@ import normalizeGradePoints from '@helpers/normalize-grade-points'
 import { maxGrade } from '@helpers/grades'
 
 export default (course) => {
-  const daysOfPadding = Math.min(21, totalDaysInCourse(course) * 0.1)
-  const realGradeExpectation = daysSoFarInCourse(course) / (totalDaysInCourse(course) - daysOfPadding) * maxGrade
+  const daysInCourse = totalDaysInCourse(course)
+  const daysSoFar = daysSoFarInCourse(course)
+  const daysLeft = daysInCourse - daysSoFar
+  const daysOfPadding = Math.min(21, daysLeft * 0.1)
+  const realGradeExpectation = daysSoFar / (daysInCourse - daysOfPadding) * maxGrade
   return normalizeGradePoints(realGradeExpectation)
 }
