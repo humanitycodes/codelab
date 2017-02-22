9cf015b6479fe4999987a08b6ccf8d12b3293f8f
Erik Gillespie <erik.gillespie@gmail.com>
2017-02-22 18:23:43 -0500

    Added script to set approvedAt fields

---
+++ backend/src/scripts/170221-set-missing-approved-dates.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase-admin'
+import { appConfig } from '../firebase-settings'
+
+const app = firebase.initializeApp(appConfig)
+
+const now = Date.now()
+
+firebase.database()
+  .ref('courses/fieldGroups/large/student')
+  .orderByChild('projectCompletions')
+  .once('value')
+  .then(allCoursesSnapshot => {
+    allCoursesSnapshot.forEach(courseSnapshot => {
+      courseSnapshot.forEach(allProjectCompletionsSnapshot => {
+        allProjectCompletionsSnapshot.forEach(projectCompletionSnapshot => {
+          const projectCompletion = projectCompletionSnapshot.val()
+          if (
+            projectCompletion.submission &&
+            projectCompletion.submission.isApproved
+          ) {
+            console.log(
+              'Updating course',
+              courseSnapshot.key,
+              'and project',
+              projectCompletionSnapshot.key
+            )
+            projectCompletion.submission.approvedAt = now
+            projectCompletionSnapshot.ref.set(projectCompletion)
+          }
+        })
+      })
+    })
+  })
+  .then(() => {
+    // The most terrifying function name to cleanly disconnect from a service ever
+    app.delete()
+  })
