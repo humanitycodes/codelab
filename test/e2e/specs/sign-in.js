c2f24e4d3df626c4a1433f4460de27864f3ec93e
Chris Fritz <chrisvfritz@gmail.com>
2017-02-24 14:58:02 -0500

    increase wait time in e2e tests to 30 seconds

--- test/e2e/specs/sign-in.js
+++ test/e2e/specs/sign-in.js
@@ -1,5 +1,6 @@
 const db = require('../helpers/db').init()
 const dgen = require('../helpers/data-generator')
+const waitTime = 30000
 
 const user = dgen.user()
 
@@ -15,7 +16,7 @@ module.exports = {
   'Sign In link exist': browser => {
     browser
       .url(browser.globals.devServerURL)
-      .waitForElementVisible('.msu-standalone-signin-container', 5000)
+      .waitForElementVisible('.msu-standalone-signin-container', 30000)
 
     browser.expect.element('a[href^=\'https://oauth.ais.msu.edu/oauth/authorize\']').to.be.present
     browser.end()
@@ -24,11 +25,11 @@ module.exports = {
   'Successful Sign In with Email shows dashboard': browser => {
     browser
       .url(`${browser.globals.devServerURL}/email-sign-in`)
-      .waitForElementVisible('button', 5000)
+      .waitForElementVisible('button', 30000)
       .setValue('input[type=text]', user.email)
       .setValue('input[type=password]', db.getDefaultPassword())
       .click('button')
-      .waitForElementVisible('.main-nav a[href^=\'/sign-out\']', 5000)
+      .waitForElementVisible('.main-nav a[href^=\'/sign-out\']', 30000)
       .end()
   }
 }
