b6d92778a0ca7be1d31230004066c9dad6542c66
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-03 16:39:14 -0400

    Remove warning from lesson learning objectives

--- frontend/src/components/lesson-form-learning-objectives.vue
+++ frontend/src/components/lesson-form-learning-objectives.vue
@@ -13,9 +13,6 @@
           <input v-model="list.item.content">
         </template>
       </OrderedEditableList>
-      <p v-if="!lesson.learningObjectives.length" class="warning">
-        Learning objectives must be defined before a lesson can be added to a course. Add a new objective above, then press enter.
-      </p>
     </div>
   </div>
 </template>
