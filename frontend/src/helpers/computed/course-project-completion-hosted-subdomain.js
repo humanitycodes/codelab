149b417770fbe78187b44bf1e6c8f4001fa466dc
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-04 16:10:02 -0400

    Use GitHub login instead of email for domain prefixes

--- frontend/src/helpers/computed/course-project-completion-hosted-subdomain.js
+++ frontend/src/helpers/computed/course-project-completion-hosted-subdomain.js
@@ -6,14 +6,12 @@ const maxChars = 30
 
 export default (course, projectCompletion) => {
   const user = userByKey(projectCompletion.students[0]['.key'])
-  const username = user.email
-    .replace(/@.+/, '')
+  const username = user.github.login
     .replace(/[^a-z0-9]+/gi, '')
     .slice(0, 15)
   const domainSuffix = projectCompletion.lessonKey
     .replace(/[^a-z0-9-]+/gi, '')
     .replace(/^-+/g, '')
     .replace(/-+$/g, '')
-    .slice(0, maxChars - username.length - 1)
-  return [username, domainSuffix].join('-').toLowerCase()
+  return [username, domainSuffix].join('-').toLowerCase().slice(0, maxChars)
 }
