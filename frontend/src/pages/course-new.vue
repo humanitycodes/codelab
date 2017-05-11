2bf1b5c072a7a624c0202cd2fc6a22b409efe568
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-11 10:34:30 -0400

    Do not wrap dropdowns in label

--- frontend/src/pages/course-new.vue
+++ frontend/src/pages/course-new.vue
@@ -3,24 +3,22 @@
     <h1>Unique Course Key</h1>
     <p class="warning">This is the unique key for the course that will appear in URLs and project repos. <strong>It cannot be changed once the course is created.</strong></p>
     <div class="key-field">
-      <label title="Department abbreviation">
-        <select v-model="prefix" name="course-prefix">
-          <option v-for="prefixOption in prefixOptions" :value="prefixOption">
-            {{ prefixOption }}
-          </option>
-        </select>
-      </label>
+      <label for="course-prefix" hidden>Department abbreviation</label>
+      <select v-model="prefix" id="course-prefix" name="course-prefix" title="Department abbreviation">
+        <option v-for="prefixOption in prefixOptions" :value="prefixOption">
+          {{ prefixOption }}
+        </option>
+      </select>
       <span>-</span>
       <label for="course-number" hidden>Couse number</label>
       <input v-model="number" id="course-number" name="course-number" placeholder="Number" title="Course number">
       <span>-</span>
-      <label title="Semester/season">
-        <select v-model="semester" name="course-semester">
-          <option v-for="(code, season) in semesterOptions" :value="code">
-            {{ season }}
-          </option>
-        </select>
-      </label>
+      <label for="course-semester" hidden>Semester / Season</label>
+      <select v-model="semester" id="course-semester" name="course-semester" title="Semester/season">
+        <option v-for="(code, season) in semesterOptions" :value="code">
+          {{ season }}
+        </option>
+      </select>
       <label for="course-year" hidden>Course year</label>
       <input v-model="year" id="course-year" name="course-year" placeholder="Year" title="Course year (2 or 4 digits)">
       <span>-</span>
