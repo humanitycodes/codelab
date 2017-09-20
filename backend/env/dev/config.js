a8b852eb115984eb9e3ddc40ff846aa3fdf1e8ec
Erik Gillespie <erik.gillespie@gmail.com>
2017-09-20 10:08:05 -0400

    Update MSU OAuth URL

--- backend/env/dev/config.js
+++ backend/env/dev/config.js
@@ -2,7 +2,7 @@ module.exports = {
   serverBaseURL: 'https://localhost:8080',
   // logRequests: true,
 
-  msuAuthBaseURL: 'https://oauth.ais.msu.edu',
+  msuAuthBaseURL: 'https://oauth.itservices.msu.edu',
   msuAuthClientID: 'OAuth-MI-MSU-Lansing-Codes-Dev',
   msuAuthClientSecret: 'RhKmT7PVykyBKnutvDYFhSYKM93xyaXysykWQs53',
 
