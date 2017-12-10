dae2368fc2ddd86f161d9c4d79bfdcfe4997edd8
Erik Gillespie <erik.gillespie@gmail.com>
2017-12-09 20:16:24 -0500

    Only show code reviews for courses the instructor is in

--- frontend/src/pages/code-reviews.vue
+++ frontend/src/pages/code-reviews.vue
@@ -1,11 +1,12 @@
 <template>
   <Layout>
     <h1>Code Reviews</h1>
-    <CodeReviews :courses="courses"/>
+    <CodeReviews :courses="availableCourses"/>
   </Layout>
 </template>
 
 <script>
+import store from '@state/store'
 import Layout from '@layouts/main'
 import CodeReviews from '@components/code-reviews'
 import { courseGetters } from '@state/helpers'
@@ -14,6 +15,14 @@ export default {
   components: {
     Layout, CodeReviews
   },
-  computed: courseGetters
+  computed: {
+    ...courseGetters,
+    availableCourses () {
+      const currentUserKey = store.state.users.currentUser.uid
+      return this.courses.filter(
+        course => course.instructorKeys.indexOf(currentUserKey) !== -1
+      )
+    }
+  }
 }
 </script>
