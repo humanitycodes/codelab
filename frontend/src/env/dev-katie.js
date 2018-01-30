dc3c32271e557337ae314122e128817353383b4a
Katie Fritz <katie@katiemfritz.com>
2018-01-30 11:37:58 -0500

    set up Katie's local Windows env, though still using generic dev DB

---
+++ frontend/src/env/dev-katie.js
@@ -0,0 +1,12 @@
+import devDefaults from './dev'
+
+export default Object.assign(devDefaults, {
+  firebaseConfig: {
+    apiKey: 'AIzaSyA6XpCJEplCascJKWpXtqOI_1eiE0JyPEY',
+    authDomain: 'msulansingcodesdev-katie.firebaseapp.com',
+    databaseURL: 'https://msulansingcodesdev-katie.firebaseio.com',
+    projectId: 'msulansingcodesdev-katie',
+    storageBucket: 'msulansingcodesdev-katie.appspot.com',
+    messagingSenderId: '844811106353'
+  }
+})
