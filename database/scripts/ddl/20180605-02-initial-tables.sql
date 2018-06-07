5406823d8fa15a41eaff58bf8de7e93df8a3fa64
Erik Gillespie <erik.gillespie@gmail.com>
2018-06-07 10:41:38 -0400

    Finished lesson and course tables

--- database/scripts/ddl/20180605-02-initial-tables.sql
+++ database/scripts/ddl/20180605-02-initial-tables.sql
@@ -19,6 +19,7 @@ CREATE TABLE app_user (
 );
 
 CREATE INDEX app_user_email_index ON app_user (email);
+
 CREATE INDEX app_user_github_login_index ON app_user (github_login);
 
 -- app_role
@@ -80,3 +81,85 @@ CREATE TABLE lesson_project_criterion (
 
 CREATE INDEX lesson_project_criterion_lesson_project_id_index
 ON lesson_project_criterion (lesson_project_id);
+
+-- lesson_prerequisite
+CREATE TABLE lesson_prerequisite (
+  lesson_id BIGINT NOT NULL,
+  prerequisite_lesson_id BIGINT NOT NULL,
+  PRIMARY KEY (lesson_id, prerequisite_lesson_id),
+  FOREIGN KEY (lesson_id) REFERENCES lesson (lesson_id),
+  FOREIGN KEY (prerequisite_lesson_id) REFERENCES lesson (lesson_id)
+);
+
+CREATE INDEX lesson_prerequisite_lesson_id_index
+ON lesson_prerequisite (lesson_id);
+
+CREATE INDEX lesson_prerequisite_prerequisite_lesson_id_index
+ON lesson_prerequisite (prerequisite_lesson_id);
+
+-- course
+CREATE TABLE course (
+  course_id BIGINT NOT NULL DEFAULT NEXTVAL('id_sequence'),
+  course_slug TEXT NOT NULL UNIQUE,
+  title TEXT NOT NULL,
+  credits INT NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL,
+  syllabus TEXT NULL,
+  PRIMARY KEY (course_id)
+);
+
+CREATE INDEX course_course_slug_index ON course (course_slug);
+
+-- course_lesson
+CREATE TABLE course_lesson (
+  course_id BIGINT NOT NULL,
+  lesson_id BIGINT NOT NULL,
+  PRIMARY KEY (course_id, lesson_id),
+  FOREIGN KEY (course_id) REFERENCES course (course_id),
+  FOREIGN KEY (lesson_id) REFERENCES lesson (lesson_id)
+);
+
+CREATE INDEX course_lesson_course_id_index ON course_lesson (course_id);
+
+CREATE INDEX course_lesson_lesson_id_index ON course_lesson (lesson_id);
+
+-- course_instructor
+CREATE TABLE course_instructor (
+  course_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  PRIMARY KEY (course_id, user_id),
+  FOREIGN KEY (course_id) REFERENCES course (course_id),
+  FOREIGN KEY (user_id) REFERENCES app_user (user_id)
+);
+
+CREATE INDEX course_instructor_course_id_index ON course_instructor (course_id);
+
+CREATE INDEX course_instructor_user_id_index ON course_instructor (user_id);
+
+-- course_student
+CREATE TABLE course_student (
+  course_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  PRIMARY KEY (course_id, user_id),
+  FOREIGN KEY (course_id) REFERENCES course (course_id),
+  FOREIGN KEY (user_id) REFERENCES app_user (user_id)
+);
+
+CREATE INDEX course_student_course_id_index ON course_student (course_id);
+
+CREATE INDEX course_student_user_id_index ON course_student (user_id);
+
+-- course_student_pending
+CREATE TABLE course_student_pending (
+  course_id BIGINT NOT NULL,
+  email TEXT NOT NULL,
+  PRIMARY KEY (course_id, email),
+  FOREIGN KEY (course_id) REFERENCES course (course_id)
+);
+
+CREATE INDEX course_student_pending_course_id_index
+ON course_student_pending (course_id);
+
+CREATE INDEX course_student_pending_email_index
+ON course_student_pending (email);
