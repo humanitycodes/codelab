21a9946971e9d2ff8185548fbf5f7da16dc43274
Erik Gillespie <erik.gillespie@gmail.com>
2018-06-30 11:25:50 -0400

    Add syncing lesson prereqs to API

---
+++ backend/src/routes/api/lessons/_helpers/sync-prerequisite-lessons.js
@@ -0,0 +1,25 @@
+export default async ({ lessonRecord, updatedLesson }) => {
+  // Create prereqs that don't already have a relationship
+  const toCreate = updatedLesson.prerequisiteLessonIds.filter(
+    prerequisiteLessonId => !lessonRecord.prerequisiteLessons.some(
+      prerequisiteLesson => prerequisiteLesson.lessonId === prerequisiteLessonId
+    )
+  )
+
+  // Delete relationships that don't exist in the updated lesson
+  const toDelete = lessonRecord.prerequisiteLessons.filter(
+    prerequisiteLesson => !updatedLesson.prerequisiteLessonIds.some(
+      prerequisiteLessonId => prerequisiteLessonId === prerequisiteLesson.lessonId
+    )
+  ).map(prerequisiteLesson => prerequisiteLesson.lessonId)
+
+  // Perform any needed deletes and inserts in tandem
+  const changes = []
+  if (toDelete.length > 0) {
+    changes.push(lessonRecord.removePrerequisiteLessons(toDelete))
+  }
+  if (toCreate.length > 0) {
+    changes.push(lessonRecord.addPrerequisiteLessons(toCreate))
+  }
+  return Promise.all(changes)
+}
