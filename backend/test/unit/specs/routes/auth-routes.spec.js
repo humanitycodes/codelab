fa017926a2088a609a64ee4d718287053bdeaa9e
Erik Gillespie <erik.gillespie@gmail.com>
2018-07-12 21:02:25 -0400

    Remove a bunch of dead code and update Node version to match dev env

--- backend/test/unit/specs/routes/auth-routes.spec.js
+++
@@ -1,7 +0,0 @@
-const routes = require(`${src}/routes/auth-routes`).config
-
-describe('auth-routes', () => {
-  it('routes GET /msu/callback', () => {
-    expect(routes).to.resolve('GET', '/msu/callback')
-  })
-})
