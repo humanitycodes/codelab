4a4736a66e043bc56d0c7fd1c05a3f88e8cd9d96
Erik Gillespie <erik.gillespie@gmail.com>
2018-07-16 14:48:35 -0400

    Move GitHub OAuth secret into environment variable

--- backend/env/staging/config.js
+++ backend/env/staging/config.js
@@ -6,7 +6,6 @@ module.exports = {
 
   githubAuthBaseURL: 'https://api.github.com',
   githubAuthClientID: '473069becdcae15254ea',
-  githubAuthClientSecret: '2973bb74e05293f369e3c9bd6ffd2bfbe259cd28',
 
   githubEventsBaseURL: 'https://msu-codes-staging.herokuapp.com/api',
   githubEventsPath: '/github-events/53ba4eac-2d46-4ce8-a8e2-1d70da0da9d2'
--- backend/env/testing/config.js
+++ backend/env/testing/config.js
@@ -6,7 +6,6 @@ module.exports = {
 
   githubAuthBaseURL: 'https://api.github.com',
   githubAuthClientID: '740ecf728a1bf799961b',
-  githubAuthClientSecret: '85174d69151083ce58a0c0775324e5f087f1a665',
 
   githubEventsBaseURL: 'https://codelab.cas.msu.edu/api',
   githubEventsPath: '/github-events/eb564a2f-fca6-41fa-8e6d-11009a330e0b'
