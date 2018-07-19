fbe60829323f5fdce2fef4c210deb771d7569e3c
Erik Gillespie <erik.gillespie@gmail.com>
2018-07-19 15:03:45 -0400

    Updated DB scripts and steps for deploying on CircleCI

--- database/scripts/ddl/20180605-01-db-and-users.sql
+++ database/scripts/ddl/20180605-01-db-and-users.sql
@@ -1,11 +1,12 @@
+\set ON_ERROR_STOP on
+
 -- Create admin role for creating tables, indexes, etc.
 CREATE USER codelab_admin;
 
 -- Create app role for accessing data
-CREATE USER codelab_app;
+\set CODELAB_DB_PASSWORD `echo ${CODELAB_DB_PASSWORD}`
+CREATE USER codelab_app WITH PASSWORD :'CODELAB_DB_PASSWORD';
 REVOKE CREATE ON SCHEMA public FROM codelab_app;
-
-\password codelab_app;
 
 -- Create database
 CREATE DATABASE codelab OWNER codelab_admin;
