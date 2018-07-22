3d0eef03c9e52192ba0832a5c2b52a4757051035
Erik Gillespie <erik.gillespie@gmail.com>
2018-07-22 16:48:07 -0400

    Add backward-compatibility for old projects

--- backend/src/helpers/github/repo-name.js
+++ backend/src/helpers/github/repo-name.js
@@ -1,4 +1,20 @@
-export default ({ courseKey, lessonKey }) => {
-  const courseParts = courseKey.split('-')
-  return `${courseParts[0]}-${courseParts[1]}-${lessonKey}`
+// Returns the GitHub repository name in one of two formats, depending on
+// whether the `projectKey` parameter is provided.
+//
+// Old format:
+// - Format:  {courseKey}-{lessonKey}-{last6of(projectKey)}
+// - Example: MI-449-SS18-740-js-create-an-api-with-express-URJy-_
+//
+// New format:
+// - Format:  {dept(courseKey)}-{classNum(courseKey)}-{lessonKey}
+// - Example: MI-449-js-create-an-api-with-express
+export default ({ courseKey, lessonKey, projectKey }) => {
+  if (projectKey && projectKey.length) {
+    // Old format, maintained for past courses
+    return `${courseKey}-${lessonKey}-${projectKey.slice(-6)}`
+  } else {
+    // New format, easier to read and type
+    const courseParts = courseKey.split('-')
+    return `${courseParts[0]}-${courseParts[1]}-${lessonKey}`
+  }
 }
--- frontend/src/helpers/computed/course-lesson-project-repo-name.js
+++ frontend/src/helpers/computed/course-lesson-project-repo-name.js
@@ -1,4 +1,20 @@
+// Returns the GitHub repository name for a given course and lesson.
+//
+// - If the lesson contains a project key, then the old style of name:
+//   `{courseKey}-{lessonKey}-{last6of(projectKey)}` will be used.
+// - If there is no project key, then the new style of name:
+//   `{dept(courseKey)}-{courseNum(courseKey)}-{lessonKey}` will be used.
+//
+// These two formats are used to support old classes (mid-2018 and earlier)
+// and classes held after mid-2018.
 export default (course, lesson) => {
-  const courseParts = course.courseKey.split('-')
-  return `${courseParts[0]}-${courseParts[1]}-${lesson.lessonKey}`
+  if (lesson.projectKey && lesson.projectKey.length) {
+    const courseKey = course.courseKey
+    const lessonKey = lesson.lessonKey
+    const projectKey = lesson.projectKey
+    return `${courseKey}-${lessonKey}-${projectKey.slice(-6)}`
+  } else {
+    const courseParts = course.courseKey.split('-')
+    return `${courseParts[0]}-${courseParts[1]}-${lesson.lessonKey}`
+  }
 }
