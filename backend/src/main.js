580ad561d48bdbe87c8d189879752be1a1b89efa
Erik Gillespie <erik.gillespie@gmail.com>
2018-07-24 21:03:01 -0400

    Only use cluster when there is more than one CPU core

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,11 +3,16 @@
 if (!global._babelPolyfill) require('babel-polyfill')
 
 import WEB_CONCURRENCY from '../env/web-concurrency'
-import throng from 'throng'
 import startWorker from './start-worker'
 
-throng({
-  workers: WEB_CONCURRENCY,
-  lifetime: Infinity,
-  start: startWorker
-})
+if (WEB_CONCURRENCY > 1) {
+  const throng = require('throng')
+
+  throng({
+    workers: WEB_CONCURRENCY,
+    lifetime: Infinity,
+    start: startWorker
+  })
+} else {
+  startWorker()
+}
