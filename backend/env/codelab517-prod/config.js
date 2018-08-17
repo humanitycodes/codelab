4d1f62188695f4ffa3d22c1207aa777d5e62eec9
Erik Gillespie <erik.gillespie@gmail.com>
2018-08-17 10:24:19 -0400

    Use www.codelab517.com for prod URL

--- backend/env/codelab517-prod/config.js
+++ backend/env/codelab517-prod/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  serverBaseURL: 'https://codelab517.com',
+  serverBaseURL: 'https://www.codelab517.com',
 
   githubAuthBaseURL: 'https://api.github.com',
   githubAuthClientID: '278ad948f2d3c5fc3f1c',
 
-  githubEventsBaseURL: 'https://codelab517.com/api/github-events'
+  githubEventsBaseURL: 'https://www.codelab517.com/api/github-events'
 }
