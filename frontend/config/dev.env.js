dc568fdd205c2f63c6b766ec0578e5b38de2ed40
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-03 13:00:55 -0400

    Make other env vars that frontend needs available during builds

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -1,4 +1,18 @@
+// -----------------------------------------------------------------------------
+// IMPORTANT:
+//   1. Changes made here may also need to be reflected in production.env.js
+//   2. Expose environment variables used in src/env/*.js
+//   3. Do not expose environment variables that contain private info!
+// -----------------------------------------------------------------------------
+
 module.exports = {
   NODE_ENV: '"development"',
-  CODELAB_BRAND: '"' + (process.env.CODELAB_BRAND || 'msu') + '"'
+  CODELAB_BRAND:
+    '"' + (process.env.CODELAB_BRAND || 'msu') + '"',
+  CODELAB_GITHUB_AUTH_CLIENT_ID:
+    '"' + (process.env.CODELAB_GITHUB_AUTH_CLIENT_ID || '740ecf728a1bf799961b') + '"',
+  CODELAB_MSU_AUTH_CLIENT_ID:
+    '"' + (process.env.CODELAB_MSU_AUTH_CLIENT_ID || 'OAuth-MI-MSU-Lansing-Codes-Dev') + '"',
+  CODELAB_SERVER_BASE_URL:
+    '"' + (process.env.CODELAB_SERVER_BASE_URL || 'https://localhost:8080') + '"'
 }
--- frontend/config/production.env.js
+++ frontend/config/production.env.js
@@ -1,4 +1,14 @@
+// -----------------------------------------------------------------------------
+// IMPORTANT:
+//   1. Changes made here may also need to be reflected in dev.env.js
+//   2. Expose environment variables used in src/env/*.js
+//   3. Do not expose environment variables that contain private info!
+// -----------------------------------------------------------------------------
+
 module.exports = {
   NODE_ENV: '"production"',
-  CODELAB_BRAND: '"' + process.env.CODELAB_BRAND + '"'
+  CODELAB_BRAND: '"' + process.env.CODELAB_BRAND + '"',
+  CODELAB_GITHUB_AUTH_CLIENT_ID: '"' + process.env.CODELAB_GITHUB_AUTH_CLIENT_ID + '"',
+  CODELAB_MSU_AUTH_CLIENT_ID: '"' + process.env.CODELAB_MSU_AUTH_CLIENT_ID + '"',
+  CODELAB_SERVER_BASE_URL: '"' + process.env.CODELAB_SERVER_BASE_URL + '"'
 }
