361bc8fa608e443c0adabe4ab353e9f7f849a5a0
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-06 12:18:01 -0400

    Make done indicator its own component

---
+++ frontend/src/components/done-indicator.vue
@@ -0,0 +1,29 @@
+<template>
+  <span class="fa-stack fa-xs">
+    <i class="far fa-square fa-stack-2x"></i>
+    <i
+      v-show="done"
+      class="fas fa-check fa-stack-1x text-green-dark"
+      :aria-label="done ? doneLabel : notDoneLabel"
+    ></i>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    done: {
+      type: Boolean,
+      required: true
+    },
+    doneLabel: {
+      type: String,
+      required: true
+    },
+    notDoneLabel: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
