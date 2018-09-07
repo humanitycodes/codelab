026b0f3af0862cb45ccf36a6b4f6722414b69da5
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-07 14:24:45 -0400

    Add done button to complete Get Started workflow

--- frontend/src/pages/get-started.vue
+++ frontend/src/pages/get-started.vue
@@ -6,6 +6,11 @@
         To have the best experience possible, please complete all of these
         tasks.
       </p>
+      <DoneButton
+        :disabled="!allTasksDone"
+        title="Press this button after completing all of the tasks."
+        @click="goBackOrFallback"
+      />
     </header>
     <Box>
       <NotificationsForm/>
@@ -15,11 +20,23 @@
 
 <script>
 import Layout from '@layouts/restricted'
+import DoneButton from '@components/done-button'
 import NotificationsForm from '@components/notifications-form'
+import { userGetters } from '@state/helpers'
+import goBackOrFallback from '@helpers/utils/go-back-or-fallback'
 
 export default {
   components: {
-    Layout, NotificationsForm
+    Layout, DoneButton, NotificationsForm
+  },
+  computed: {
+    ...userGetters,
+    allTasksDone () {
+      return this.hasMessagingToken
+    }
+  },
+  methods: {
+    goBackOrFallback
   }
 }
 </script>
