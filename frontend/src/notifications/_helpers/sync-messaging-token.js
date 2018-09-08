a7975a9d8688c8dfc7be12b0b3f8d77814ea839c
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-07 20:11:56 -0400

    Basic notification handler in place

--- frontend/src/notifications/_helpers/sync-messaging-token.js
+++ frontend/src/notifications/_helpers/sync-messaging-token.js
@@ -3,18 +3,16 @@ import store from '@state/store'
 import updateUserMessagingToken from '@api/users/update-user-messaging-token'
 
 export default () => {
-  return messaging.getToken()
-    .then(messagingToken => {
-      const userId = store.state.users.currentUser.userId
-      return updateUserMessagingToken({ userId, messagingToken })
-        .then(() => {
-          if (!messagingToken) {
-            throw new Error(
-              'Unable to retrieve a messaging token. ' +
-              'This usually happens because permission to allow push ' +
-              'notifications must be requested.'
-            )
-          }
-        })
+  return messaging.getToken().then(messagingToken => {
+    const userId = store.state.users.currentUser.userId
+    return updateUserMessagingToken({ userId, messagingToken }).then(() => {
+      if (!messagingToken) {
+        throw new Error(
+          'Unable to retrieve a messaging token. ' +
+          'This usually happens because permission to allow push ' +
+          'notifications must be requested.'
+        )
+      }
     })
+  })
 }
