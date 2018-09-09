c04877a6e681b0a1192ce90bfe394b14421e6166
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-08 20:15:41 -0400

    Disable notifications for end-to-end tests to avoid the blocking popup

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -41,7 +41,10 @@ module.exports = {
 
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: 'chrome',
+        chromeOptions: {
+          args: ['--disable-notifications']
+        }
       }
     },
 
