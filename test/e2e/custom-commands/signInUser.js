895be138e5b3cfe5a288f2bbf8ecf58f478d97c1
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-12 14:46:46 -0400

    Fake out messaging tokens in e2e tests

--- test/e2e/custom-commands/signInUser.js
+++ test/e2e/custom-commands/signInUser.js
@@ -6,6 +6,11 @@ exports.command = function (user) {
     .url(this.launchUrl)
     .waitForElementVisible('.msu-standalone-signin-container', waitTime)
     .setLocalStorage('auth_token', signJsonWebToken({ user }))
+    .setLocalStorage('user_messaging_token', JSON.stringify({
+      userMessagingTokenId: 1,
+      userId: user.userId,
+      messagingToken: 'e2e-messaging-token'
+    }))
     .url(this.launchUrl)
     .waitForElementVisible('.main-nav a[href^=\'/sign-out\']', waitTime)
 }
