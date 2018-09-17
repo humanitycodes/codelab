d46166028744145cdd40069998ff67027ad575a6
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-17 18:34:29 -0400

    Send resource id if too big to send in push notification

--- backend/src/notifications/courses/_helpers/broadcast-course-data-message.js
+++ backend/src/notifications/courses/_helpers/broadcast-course-data-message.js
@@ -1,5 +1,6 @@
 import translateCourseFromRecord from 'translators/course/from-record'
 import broadcastDataToUser from 'notifications/_helpers/broadcast-data-to-user'
+import isObjectTooBigToBroadcast from 'notifications/_helpers/is-object-too-big-to-broadcast'
 
 export default async ({ action, courseRecord, recipientUserRecords }) =>
   Promise.all(
@@ -11,13 +12,16 @@ export default async ({ action, courseRecord, recipientUserRecords }) =>
           courseRecord
         })
 
+      const data = { action, resourceType: 'course' }
+      if (isObjectTooBigToBroadcast(course)) {
+        data.resourceId = course.courseId
+      } else {
+        data.resource = course
+      }
+
       return broadcastDataToUser({
         userId: userRecord.userId,
-        data: {
-          action,
-          resourceType: 'course',
-          resource: course
-        }
+        data
       })
     })
   )
--- frontend/src/notifications/_helpers/handle-messages.js
+++ frontend/src/notifications/_helpers/handle-messages.js
@@ -1,26 +1,56 @@
 import messaging from '../messaging'
 import store from '@state/store'
 
+// Sample payloads:
+// ----------------
+// {
+//   data: {
+//     action: 'created',
+//     resourceType: 'lesson',
+//     resourceId: 432
+//   }
+// }
+//
+// {
+//   data: {
+//     action: 'updated',
+//     resourceType: 'project-completion',
+//     resource: {
+//       projectCompletionId: 8745,
+//       lessonId: 432,
+//       courseId: 65,
+//       ...
+//     }
+//   }
+// }
 export default () => {
   return Promise.resolve(
     messaging.onMessage(payload => {
       // Firebase handles notification-only messages
       if (!payload.data) return
 
-      const { action, resourceType } = payload.data
-      const resource = JSON.parse(payload.data.resource)
+      const { action, resourceType, resourceId } = payload.data
+      const resource = resourceId ? null : JSON.parse(payload.data.resource)
 
       if (resourceType === 'course') {
         if (action === 'created' || action === 'updated') {
-          store.dispatch('mergeCourses', [resource])
+          if (resourceId) {
+            store.dispatch('syncCourse', resourceId)
+          } else {
+            store.dispatch('mergeCourses', [resource])
+          }
         } else if (action === 'deleted') {
-          store.dispatch('removeCourses', [resource.courseId])
+          store.dispatch('removeCourses', [resourceId || resource.courseId])
         }
       } else if (resourceType === 'lesson') {
         if (action === 'created' || action === 'updated') {
-          store.dispatch('mergeLessons', [resource])
+          if (resourceId) {
+            store.dispatch('syncLesson', resourceId)
+          } else {
+            store.dispatch('mergeLessons', [resource])
+          }
         } else if (action === 'deleted') {
-          store.dispatch('removeLessons', [resource.lessonId])
+          store.dispatch('removeLessons', [resourceId || resource.lessonId])
         }
       } else if (resourceType === 'project-completion') {
         if (action === 'created' || action === 'updated') {
