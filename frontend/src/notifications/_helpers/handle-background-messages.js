2a9d528a8272a4df31e8d592889837309efc4a77
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-19 16:04:43 -0400

    Save background data messages to IndexedDB

--- frontend/src/notifications/_helpers/handle-background-messages.js
+++ frontend/src/notifications/_helpers/handle-background-messages.js
@@ -1,7 +1,28 @@
+import { set } from 'idb-keyval'
 import messaging from '../messaging'
 
 if (messaging) {
   messaging.setBackgroundMessageHandler(payload => {
-    console.log('payload:', payload)
+    if (!payload.data) return
+
+    const { resourceType, resourceId, resource } = payload.data
+    if (resourceId) {
+      set(resourceId, resourceType)
+    } else {
+      switch (resourceType) {
+        case 'course':
+          set(resource.courseId, resourceType)
+          break
+        case 'lesson':
+          set(resource.lessonId, resourceType)
+          break
+        case 'project-completion':
+          set(resource.projectCompletionId, resourceType)
+          break
+        case 'user':
+          set(resource.userId, resourceType)
+          break
+      }
+    }
   })
 }
