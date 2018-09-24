c3ee11344e0d6d5341952babb3f3c6e873565cb2
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-23 21:34:23 -0400

    Remove log statement

--- backend/src/notifications/messaging.js
+++ backend/src/notifications/messaging.js
@@ -2,10 +2,11 @@ import firebaseAdmin from 'firebase-admin'
 import firebaseAdminPrivateKeyJson from '../../env/firebase-admin-private-key-json'
 import firebaseProjectId from '../../env/firebase-project-id'
 
+// Some managed hosting environments like Heroku escape newlines in
+// environment variables, so explicitly unescape them
 const firebaseCredential = JSON.parse(firebaseAdminPrivateKeyJson)
 const privateKey = firebaseCredential.private_key
 firebaseCredential.private_key = privateKey.replace(/\\n/g, '\n')
-console.error('Firebase Credential:', firebaseCredential)
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(firebaseCredential),
