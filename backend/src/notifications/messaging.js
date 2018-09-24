b1b0cb5c6c62ea90e0ef72ae1b03935710b5952e
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-23 21:20:10 -0400

    Fix newlines in private key

--- backend/src/notifications/messaging.js
+++ backend/src/notifications/messaging.js
@@ -2,11 +2,12 @@ import firebaseAdmin from 'firebase-admin'
 import firebaseAdminPrivateKeyJson from '../../env/firebase-admin-private-key-json'
 import firebaseProjectId from '../../env/firebase-project-id'
 
-const firebaseAdminPrivateKey = JSON.parse(firebaseAdminPrivateKeyJson)
-console.error('Firebase Credential:', firebaseAdminPrivateKey)
+const firebaseCredential = JSON.parse(firebaseAdminPrivateKeyJson)
+firebaseCredential.private_key = firebaseCredential.replace(/\\n/g, '\n')
+console.error('Firebase Credential:', firebaseCredential)
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(firebaseAdminPrivateKey),
+  credential: firebaseAdmin.credential.cert(firebaseCredential),
   projectId: firebaseProjectId
 })
 
