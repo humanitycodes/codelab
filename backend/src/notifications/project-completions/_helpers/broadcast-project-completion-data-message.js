5a6218b02a155c666c6a84bb6964da954a3d9f2b
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-28 09:46:28 -0400

    Broadcast ID or whole project completion based on size and make sure values are all strings

--- backend/src/notifications/project-completions/_helpers/broadcast-project-completion-data-message.js
+++ backend/src/notifications/project-completions/_helpers/broadcast-project-completion-data-message.js
@@ -1,5 +1,6 @@
 import translateProjectCompletionFromRecord from 'translators/project-completion/from-record'
 import broadcastDataToUser from 'notifications/_helpers/broadcast-data-to-user'
+import isStringTooBigToBroadcast from 'notifications/_helpers/is-string-too-big-to-broadcast'
 
 export default async ({
   action,
@@ -12,10 +13,12 @@ export default async ({
       projectCompletionRecord
     })
 
-  const data = {
-    action,
-    resourceType: 'project-completion',
-    resource: projectCompletion
+  const data = { action, resourceType: 'project-completion' }
+  const projectCompletionJson = JSON.stringify(projectCompletion)
+  if (isStringTooBigToBroadcast(projectCompletionJson)) {
+    data.resourceId = projectCompletion.projectCompletionId.toString()
+  } else {
+    data.resource = projectCompletionJson
   }
 
   return Promise.all(
