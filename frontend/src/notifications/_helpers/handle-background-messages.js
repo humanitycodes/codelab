c3988feee16f2253712caff3c8068fcd6357092f
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-30 11:00:03 -0400

    Get resource ID from resource if not provided in payload

--- frontend/src/notifications/_helpers/handle-background-messages.js
+++ frontend/src/notifications/_helpers/handle-background-messages.js
@@ -6,12 +6,23 @@ if (messaging) {
   messaging.setBackgroundMessageHandler(payload => {
     console.log('Got background message:', payload)
 
-    const resourceId = getResourceIdFromPayload(payload)
-    if (isNaN(resourceId)) return
+    const { resourceType } = payload.data
+    let resourceId = getResourceIdFromPayload(payload)
+    if (isNaN(resourceId)) {
+      if (resourceType === 'course') {
+        resourceId = payload.resource.courseId
+      } else if (resourceType === 'lesson') {
+        resourceId = payload.resource.lessonId
+      } else if (resourceType === 'project-completion') {
+        resourceId = payload.resource.projectCompletionId
+      } else {
+        console.warn('Unrecognized resource type:', resourceType)
+        return
+      }
+    }
 
     // Save the timestamp that this resource changed so the app can
     // re-sync data when it becomes active
-    const { resourceType } = payload.data
     const timestamp = Date.now()
     set(resourceId, { resourceType, timestamp })
   })
