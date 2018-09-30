07c8a8b74c6b138588ff7d7cff873d2c91bddd18
Erik Gillespie <erik.gillespie@gmail.com>
2018-09-30 19:28:28 -0400

    Get resource ID from parsed resource JSON

--- frontend/src/notifications/_helpers/handle-background-messages.js
+++ frontend/src/notifications/_helpers/handle-background-messages.js
@@ -6,26 +6,13 @@ if (messaging) {
   messaging.setBackgroundMessageHandler(payload => {
     console.log('Got background message:', payload)
 
-    const { resourceType } = payload.data
-    let resourceId = getResourceIdFromPayload(payload)
-    if (isNaN(resourceId)) {
-      const { resource } = payload.data
-      if (resourceType === 'course') {
-        resourceId = resource.courseId
-      } else if (resourceType === 'lesson') {
-        resourceId = resource.lessonId
-      } else if (resourceType === 'project-completion') {
-        resourceId = resource.projectCompletionId
-      } else {
-        console.warn('Unrecognized resource type:', resourceType)
-        return
-      }
-    }
+    const resourceId = getResourceIdFromPayload(payload)
+    if (isNaN(resourceId)) return
 
     // Save the timestamp that this resource changed so the app can
     // re-sync data when it becomes active
     const timestamp = Date.now()
-    console.log('Setting', resourceId.toString(), 'to', { resourceType, timestamp })
+    const { resourceType } = payload.data
     set(resourceId.toString(), { resourceType, timestamp })
   })
 }
