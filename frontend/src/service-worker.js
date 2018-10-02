a42ebfb633f628d967831ebaf57206bba0ac6ca1
Erik Gillespie <erik.gillespie@gmail.com>
2018-10-02 16:02:42 -0400

    Install and activate service worker updates right away

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,1 +1,14 @@
 import '@notifications/_helpers/handle-background-messages'
+
+// More info about service worker update lifecycle:
+// https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle#updates
+
+// Claim all tabs right away (instead of after next page reload)
+self.addEventListener('activate', event => {
+  self.clients.claim()
+})
+
+// When a new service worker has been installed, activate it immediately
+self.addEventListener('install', event => {
+  self.skipWaiting()
+})
