2f0e5f271a1d398b9b0d28ee5aa1527493d05549
Erik Gillespie <erik.gillespie@gmail.com>
2018-10-19 10:19:15 -0400

    Download complete current lesson after syncing all lessons

--- frontend/src/state/modules/lessons.js
+++ frontend/src/state/modules/lessons.js
@@ -2,7 +2,6 @@ import getLesson from '@api/lessons/get-lesson'
 import getLessons from '@api/lessons/get-lessons'
 import mergeByIdAndVersion from './_helpers/merge-by-id-and-version'
 import removeById from './_helpers/remove-by-id'
-import lessonFullyLoaded from './_helpers/lesson-fully-loaded'
 
 export default {
   state: {
@@ -44,29 +43,24 @@ export default {
           throw error
         })
     },
-    syncAllLessons ({ commit, state }) {
+    syncAllLessons ({ commit, getters }) {
       // Get all available lessons from API and save them in the local state
       return getLessons()
         .then(lessons => {
-          // Get the complete lesson again if it was previously retrieved
-          const loadedLessonIds = state.all.filter(lesson => {
-            const lessonExists = lessons.some(
-              existingLesson => existingLesson.lessonId === lesson.lessonId
-            )
-            const fullyLoaded = lessonFullyLoaded(lesson)
-            return lessonExists && fullyLoaded
-          })
-          // Re-download the loaded lessons and merge them with everything else
-          return Promise.all(loadedLessonIds.map(
-            lessonId => getLesson(lessonId)
-          ))
-          .then(loadedLessons => {
-            const mergedLessons = mergeByIdAndVersion(
-              'lessonId', lessons, loadedLessons
-            )
-            commit('SET_ALL_LESSONS', mergedLessons)
-            return mergedLessons
-          })
+          // Download the complete current lesson before saving
+          return Promise.resolve(getters.currentLesson)
+            .then(currentLesson => {
+              if (!currentLesson) return lessons
+              return getLesson(currentLesson.lessonId)
+                .then(completeCurrentLesson => mergeByIdAndVersion(
+                  'lessonId', lessons, [completeCurrentLesson]
+                ))
+                .catch(() => lessons)
+            })
+            .then(newLessons => {
+              commit('SET_ALL_LESSONS', newLessons)
+              return newLessons
+            })
         })
     },
     mergeLessons ({ commit, state }, lessons) {
