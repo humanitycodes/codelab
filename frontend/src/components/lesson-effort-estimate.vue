a37f9c9e8fc949a8d3d335e6290c1c693c99cac9
Erik Gillespie <erik.gillespie@gmail.com>
2018-10-25 16:01:11 -0400

    Describe t-shirt size estimates

--- frontend/src/components/lesson-effort-estimate.vue
+++ frontend/src/components/lesson-effort-estimate.vue
@@ -1,15 +1,31 @@
 <template>
-  <div v-if="estimatedEffort">
+  <div
+    v-if="estimatedEffort"
+    :title="
+      'This lesson is likely to take a ' +
+      estimatedEffort.full +
+      ' amount of time to complete.'
+    "
+  >
     <span class="fas fa-tshirt"/>
-    {{ estimatedEffort }}
+    {{ estimatedEffort.abbreviation }}
   </div>
 </template>
 
 <script>
 const estimateMap = {
-  1: 'S',
-  2: 'M',
-  3: 'L'
+  1: {
+    abbreviation: 'S',
+    full: 'small'
+  },
+  2: {
+    abbreviation: 'M',
+    full: 'medium'
+  },
+  3: {
+    abbreviation: 'L',
+    full: 'large'
+  }
 }
 
 export default {
