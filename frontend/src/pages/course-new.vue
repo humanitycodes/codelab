1da361039a5fb597f3deb8963323ed86c5c5431b
Eric Channey <ericchanney@gmail.com>
2018-10-27 13:50:14 -0400

    Create Duplicate Link| Added Course Search By Key|Create and Update New Course

--- frontend/src/pages/course-new.vue
+++ frontend/src/pages/course-new.vue
@@ -1,5 +1,24 @@
 <template>
   <Layout>
+    <div v-if="createDuplicate" class="mb-4">
+      <h1 class="mb-4">Select Course To Duplicate</h1>
+      <p class="danger" v-if="duplicateKeyInvalid">
+        You have provided an invalid course key.
+      </p>
+        <Dropdown
+          :results="queryResults"
+          :resultHandler="addCourse"
+          :resultContent="courseKey"
+        >
+          <input
+            ref="queryInput"
+            v-model="courseQuery"
+            id="course-duplicate-query"
+            name="course-duplicate-query"
+            placeholder="Course Key"
+          >
+        </Dropdown>
+    </div>
     <h1>Unique Course Key</h1>
     <p class="warning">
       This is the unique key for the course that will appear in URLs and projects.
@@ -60,7 +79,7 @@
       >
     </div>
     <button
-      :disabled="!keyIsValid"
+      :disabled="!keyIsValid || !this.courseKeyFormatted(this.courseQuery)"
       @click="tryToCreateCourse"
       class="primary block"
     >
@@ -73,12 +92,25 @@
 import Layout from '@layouts/main'
 import store from '@state/store'
 import { userGetters, courseGetters } from '@state/helpers'
+import { canReadCourse } from '@state/auth/courses'
 import createCourse from '@api/courses/create-course'
+import updateCourse from '@api/courses/update-course'
+import Dropdown from '../components/dropdown'
 import brand from '@env/brand'
 
 export default {
+  beforeRouteEnter (to, from, next) {
+    if (to.path === '/courses/new/duplicate') {
+      next(vm => {
+        // access to component instance via `vm`
+        vm.createDuplicate = true
+      })
+    } else {
+      next()
+    }
+  },
   components: {
-    Layout
+    Dropdown, Layout
   },
   data () {
     const prefixOptions = brand === 'msu' ? ['MI'] : ['LAB']
@@ -94,12 +126,28 @@ export default {
       number: '',
       semester: semesterOptions.Spring,
       year: '',
-      section: ''
+      section: '',
+      createDuplicate: false,
+      courseQuery: '',
+      duplicateKeyInvalid: false
     }
   },
   computed: {
     ...userGetters,
     ...courseGetters,
+    queryResults () {
+      if (!this.courseQuery || this.courseKeyFormatted(this.courseQuery)) return []
+      const queryRegex = new RegExp(this.courseQuery, 'i')
+      return this.courses.filter(course => (
+        canReadCourse({
+          courseKey: course.courseKey
+        }) &&
+        // Course matches the query string
+        (
+          queryRegex.test(course.courseKey)
+        )
+      ))
+    },
     key () {
       return [
         this.prefix,
@@ -137,16 +185,53 @@ export default {
     }
   },
   methods: {
+    courseKey (course) {
+      return course.courseKey
+    },
+    courseKeyFormatted (key) {
+      let pattern = new RegExp('[A-Z]{2,3}-[0-9]{3}-[A-Z]{2}[0-9]{2}-[0-9]{3}')
+      return pattern.test(key)
+    },
     tryToCreateCourse () {
       if (this.keyIsValid) {
         const courseKey = this.key
-        createCourse({
-          courseKey,
-          instructors: [{ userId: this.currentUser.userId }]
-        })
-        .then(newCourse => store.dispatch('mergeCourses', [newCourse]))
-        .then(() => this.$router.replace(`/courses/${courseKey}/edit`))
+        if (!this.createDuplicate) {
+          createCourse({
+            courseKey,
+            instructors: [{ userId: this.currentUser.userId }]
+          })
+          .then(newCourse => store.dispatch('mergeCourses', [newCourse]))
+          .then(() => this.$router.replace(`/courses/${courseKey}/edit`))
+        } else {
+          // Check if course to duplicate exists
+          if (!canReadCourse({ courseKey: this.courseQuery })) {
+            this.duplicateKeyInvalid = true
+            this.$refs.queryInput.focus()
+          } else {
+            this.duplicateKeyInvalid = false
+            // Get course by key
+            let duplicateCourse = this.courses.find(
+              course => course.courseKey === this.courseQuery
+            )
+            createCourse({
+              courseKey,
+              instructors: [{ userId: this.currentUser.userId }]
+            })
+            .then(newCourse => updateCourse({
+              ...newCourse,
+              title: duplicateCourse.title || null,
+              credits: duplicateCourse.credits || null,
+              syllabus: duplicateCourse.syllabus || null,
+              lessonIds: duplicateCourse.lessonIds || null
+            }))
+            .then(newCourse => store.dispatch('mergeCourses', [newCourse]))
+            .then(() => this.$router.replace(`/courses/${courseKey}/edit`))
+          }
+        }
       }
+    },
+    addCourse (course) {
+      this.courseQuery = course.courseKey
     }
   }
 }
