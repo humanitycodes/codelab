48e53df27854c6778a0e770ae2a2efd2cf3811b0
Eric Channey <ericchanney@gmail.com>
2018-10-27 14:32:20 -0400

    Moved dropdown styles into app.scss and made dynamic to fit theme

--- frontend/src/components/dropdown.vue
+++ frontend/src/components/dropdown.vue
@@ -50,34 +50,3 @@ export default {
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-@import '../meta'
-
-.dropdown-wrapper
-  position: relative
-
-  &.expanded input
-    border-bottom-left-radius: 0
-    border-bottom-right-radius: 0
-    border-bottom-color: lighten($design.branding.primary.light, 80%)
-
-.dropdown-results
-  position: absolute
-  width: 100%
-  left: 0
-  top: calc(100% + $design.control.padding.vertical)
-  background-color: $design.branding.muted.light.tan
-  border: 1px solid $design.branding.primary.light
-  border-top: none
-  border-bottom-left-radius: $design.control.border.radius
-  border-bottom-right-radius: $design.control.border.radius
-  z-index: 1
-
-.dropdown-result
-  padding: $design.control.padding.vertical $design.control.padding.horizontal
-  cursor: pointer
-  &.focused
-    background-color: $design.branding.primary.light
-    color: white
-</style>
