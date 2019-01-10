b133373ce3bb5556a20cd26e0525ba0694a33962
Erik Gillespie <erik.gillespie@gmail.com>
2019-01-10 10:16:05 -0500

    Remove check for private repos -- GitHub allows all users to do this now!

--- frontend/src/components/project-instructions-unstarted.vue
+++ frontend/src/components/project-instructions-unstarted.vue
@@ -1,39 +1,24 @@
 <template>
-  <div v-if="!userPrivileges">
-    <p v-if="error" class="error">{{ error }}</p>
+  <div v-if="!allPrereqsComplete()">
+    <p class="warning">
+      This project cannot be started until all prerequisite lessons have been completed.
+    </p>
   </div>
   <div v-else>
-    <div v-if="!userPrivileges.createPrivateRepo">
-      <p class="warning">
-        Projects cannot be started until you are able to create private repositories in GitHub.
-        Apply for the <a href="https://education.github.com/pack" target="_blank">Student Developer Pack</a>
-        and after your GitHub account has been upgraded, try starting the project again.
-        <button class="warning block" @click="fetchPrivileges">
-          Check Again
-        </button>
-      </p>
-    </div>
-    <div v-else-if="!allPrereqsComplete()">
-      <p class="warning">
-        This project cannot be started until all prerequisite lessons have been completed.
-      </p>
-    </div>
-    <div v-else>
-      <p v-if="error" class="error">{{ error }}</p>
-      <button
-        v-if="!projectCompletion"
-        class="block"
-        @click="createProjectRepo"
-        :disabled="starting"
-      >
-        <span v-if="starting">
-          Just a few seconds...
-        </span>
-        <span v-else>
-          Start Project
-        </span>
-      </button>
-    </div>
+    <p v-if="error" class="error">{{ error }}</p>
+    <button
+      v-if="!projectCompletion"
+      class="block"
+      @click="createProjectRepo"
+      :disabled="starting"
+    >
+      <span v-if="starting">
+        Just a few seconds...
+      </span>
+      <span v-else>
+        Start Project
+      </span>
+    </button>
   </div>
 </template>
 
@@ -42,7 +27,6 @@ import store from '@state/store'
 import areAllPrereqsComplete from '@helpers/computed/course-lesson-user-prereqs-are-all-complete'
 import { userGetters } from '@state/helpers'
 import createProjectCompletion from '@api/project-completions/create-project-completion'
-import getUserPrivileges from '@api/users/get-user-privileges'
 
 export default {
   props: {
@@ -59,12 +43,8 @@ export default {
   data () {
     return {
       error: '',
-      starting: false,
-      userPrivileges: null
+      starting: false
     }
-  },
-  created () {
-    this.fetchPrivileges()
   },
   computed: userGetters,
   methods: {
@@ -89,31 +69,6 @@ export default {
           your instructor about this and we'll work to resolve it as soon as
           possible.
         `
-      })
-    },
-    fetchPrivileges () {
-      this.userPrivileges = null
-      getUserPrivileges({ userId: this.currentUser.userId })
-      .then(privileges => {
-        this.error = ''
-        this.userPrivileges = privileges
-      })
-      .catch(error => {
-        if (error.response && error.response.status === 404) {
-          this.error = `
-            There was a problem looking up your GitHub plan. Sign out and sign
-            in before trying again. If you still can't start the project, tell
-            your instructor about this and we'll work to resolve it as soon as
-            possible.
-          `
-        } else {
-          this.error = `
-            There was a problem looking up your GitHub plan. Make sure you're
-            connected to the Internet. If you've confirmed you are, tell your
-            instructor about this and we'll work to resolve it as soon as
-            possible.
-          `
-        }
       })
     }
   }
