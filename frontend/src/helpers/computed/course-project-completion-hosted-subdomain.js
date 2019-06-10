058f95dd4dc686b9f6a6c22e03f1ee04e9b00b3b
Erik Gillespie <erik.gillespie@gmail.com>
2019-06-09 20:26:41 -0400

    Add comments for all of the string mutations

--- frontend/src/helpers/computed/course-project-completion-hosted-subdomain.js
+++ frontend/src/helpers/computed/course-project-completion-hosted-subdomain.js
@@ -8,18 +8,25 @@ const maxChars = 30
 export default projectCompletion => {
   const user = userById(projectCompletion.studentUserId)
   const username = user.githubLogin
+    // remove non-alphanumeric characters
     .replace(/[^a-z0-9]+/gi, '')
+    // trim to 15 characters or less
     .slice(0, 15)
 
   const lesson = lessonById(projectCompletion.lessonId)
   const domainSuffix = lesson.lessonKey
+    // remove characters that aren't alphanumeric or a hyphen
     .replace(/[^a-z0-9-]+/gi, '')
+    // remove leading hyphens
     .replace(/^-+/g, '')
+    // remove trailing hyphens
     .replace(/-+$/g, '')
 
   const subdomain = `${username}-${domainSuffix}`
     .toLowerCase()
+    // ensure the string does not exceed the max length
     .slice(0, maxChars)
+    // remove special characters from the end
     .replace(/[^a-z0-9]+$/gi, '')
 
   return subdomain
