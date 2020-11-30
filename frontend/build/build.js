2e9e9288dc2111474704d0d6367d80b8d6424bab
Erik Gillespie <erik.gillespie@gmail.com>
2020-11-29 21:28:57 -0500

    Remove whole ./dist dir before building

--- frontend/build/build.js
+++ frontend/build/build.js
@@ -13,7 +13,7 @@ const webpackConfig = require('./webpack.prod.conf')
 const spinner = ora(`building for ${process.env.NODE_ENV}...`)
 spinner.start()
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+rm(config.build.assetsRoot, err => {
   if (err) throw err
   webpack(webpackConfig, (err, stats) => {
     spinner.stop()
