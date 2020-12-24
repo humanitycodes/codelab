54e8e9e9324d1bbbca5d43b2aa04fa10f95ffb8d
Erik Gillespie <erik.gillespie@gmail.com>
2020-12-24 12:10:28 -0500

    Add readonly email to user profile page

---
+++ frontend/src/components/user-form-email.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="stretch-row">
+    <div class="stretch-col">
+      <label for="user-email">Email</label>
+      <input
+        :value="email"
+        id="user-email"
+        name="user-email"
+        title="Email address cannot be changed"
+        readonly
+        aria-readonly="true"
+        disabled
+        aria-disabled="true"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    email: String
+  }
+}
+</script>
--- frontend/src/components/user-form.vue
+++ frontend/src/components/user-form.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
+    <UserEmail :email="email"/>
     <UserFullName v-on="$listeners" :full-name="fullName"/>
   </div>
 </template>
 
 <script>
+import UserEmail from './user-form-email'
 import UserFullName from './user-form-full-name'
 
 export default {
   components: {
-    UserFullName
+    UserEmail, UserFullName
   },
   props: {
+    email: String,
     fullName: String
   }
 }
