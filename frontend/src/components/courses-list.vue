3ffaeb6f4de8789bc08d45e771b29e9cb594668d
Erik Gillespie <erik.gillespie@gmail.com>
2021-01-18 22:42:51 -0500

    Factor expand/collapse functionality into new component

--- frontend/src/components/courses-list.vue
+++ frontend/src/components/courses-list.vue
@@ -1,48 +1,32 @@
 <template>
-  <section>
-    <h2
-      aria-haspopup="listbox"
-      class="mt-4 mb-2 cursor-pointer"
-      @click="showList = !showList"
-    >
-      <span
-        :class="showList ? 'fa-caret-down' : 'fa-caret-right'"
-        class="fas fa-fw"
-        role="img"
-      ></span>
-      {{ title }}
-    </h2>
-    <transition name="fade">
-      <div v-show="showList" :aria-expanded="showList || 'false'">
-        <p v-if="!orderedCourses.length" class="pl-5 italic">
-          This course list is empty.
-        </p>
-        <ul v-else>
-          <li
-            v-for="course in orderedCourses"
-            :key="course.courseKey"
-          >
-            <router-link
-              v-if="canUpdateCourse({ courseKey: course.courseKey })"
-              :to="'/courses/' + course.courseKey + '/edit'"
-            >
-              <button
-                class="inline"
-                :disabled="!isInstructorInCourse(course)"
-                :title="titleForCourseEditButton(course)"
-              >Edit</button>
-            </router-link>
-            <router-link :to="'/courses/' + course.courseKey">
-              {{ course.courseKey }}
-              <span v-if="course.title">
-                ({{ course.title }})
-              </span>
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </transition>
-  </section>
+  <div>
+    <p v-if="!orderedCourses.length" class="pl-5 italic">
+      This course list is empty.
+    </p>
+    <ul v-else>
+      <li
+        v-for="course in orderedCourses"
+        :key="course.courseKey"
+      >
+        <router-link
+          v-if="canUpdateCourse({ courseKey: course.courseKey })"
+          :to="'/courses/' + course.courseKey + '/edit'"
+        >
+          <button
+            class="inline"
+            :disabled="!isInstructorInCourse(course)"
+            :title="titleForCourseEditButton(course)"
+          >Edit</button>
+        </router-link>
+        <router-link :to="'/courses/' + course.courseKey">
+          {{ course.courseKey }}
+          <span v-if="course.title">
+            ({{ course.title }})
+          </span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -51,22 +35,9 @@ import orderBy from 'lodash/orderBy'
 
 export default {
   props: {
-    title: {
-      type: String,
-      required: true
-    },
     courses: {
       type: Array,
       required: true
-    },
-    expanded: {
-      type: Boolean,
-      default: true
-    }
-  },
-  data () {
-    return {
-      showList: this.expanded
     }
   },
   computed: {
---
+++ frontend/src/components/expander.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h2
+      aria-haspopup="listbox"
+      class="mt-4 mb-2 cursor-pointer"
+      @click="showContent = !showContent"
+    >
+      <span
+        :class="showContent ? 'fa-caret-down' : 'fa-caret-right'"
+        class="fas fa-fw"
+        role="img"
+      ></span>
+      {{ title }}
+    </h2>
+    <transition name="fade">
+      <div v-show="showContent" :aria-expanded="showContent || 'false'">
+        <slot></slot>
+      </div>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    expanded: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      showContent: this.expanded
+    }
+  }
+}
+</script>
